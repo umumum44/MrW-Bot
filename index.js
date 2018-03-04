cad8c4e4bce60fba5f194a14ac2ca7538fc8f312
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 13:25:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ fs.readdir("./commands/", (err, files) => {
 });
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity("!ping", {type: "PLAYING"});
+  bot.user.setActivity("Games", {type: "PLAYING"});
 });
 
 bot.on("message", async message => {
