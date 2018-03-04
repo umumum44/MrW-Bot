1fba71dd284723ce0f98ef7219b7cc447716b62c
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 12:02:27 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -7,10 +7,12 @@ if(message.member.hasPermission("MANAGE_MESSAGES")) {
   if(!num) return message.channel.send("You must provide the number of messages to delete!")
   if(num > 100) return message.channel.send("You can only purge 100 messages at a time!")
 message.channel.bulkDelete(args[0])
-.then(messages => let aa = message.channel.send(`Deleted ${messages.size} messages that were not over two weeks old!`))
+.then(messages => {
+  let aa = message.channel.send(`Deleted ${messages.size} messages that were not over two weeks old!`))
+  aa.delete(5000)
+}
   .catch(console.error);
 } else return message.channel.send(`${message.author}, you do not have permission to purge messages!`)
-aa.delete(5000)
 }
 
 module.exports.help = {
