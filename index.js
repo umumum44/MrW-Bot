96f1bf6b412852582fc69388f29be27ec021a1dc
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 15:42:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const botconfig = require("./botconfig.js");
 const Discord = require("discord.js");
 const fs = require("fs");
+const pg = require("pg");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
