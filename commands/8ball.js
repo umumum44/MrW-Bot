b40b5a0185ddb86b6639812d7ac54e882d818a5d
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 13:21:08 -0500

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   var responses = ["no.","highly unlikely.","very doubtful.","you wish.","don't count on it", "probably.", "most definitely", "yes.", "highly likely.", "of course.",
                   "ask again later.", "outlook not so good, retry.", "ask again later."];
   var randomchoice = Math.floor(Math.random() * responses.length);
-  message.reply("The 8ball says..."+responses[responses]);
+  message.reply("The 8ball says..."+responses[randomchoice]);
 }
 module.exports.help = {
   name: "8ball"
