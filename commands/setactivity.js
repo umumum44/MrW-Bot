99c329ad012f4a112dfe5e6b3942bdf6b6a038d6
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 12:07:41 -0500

    Create setactivity.js

---
+++ commands/setactivity.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+let tbh = args.join(" ").toUpperCase()
+
+if(message.author.id === "399975738008141824") {
+bot.user.setActivity(`${bot.user.presence.game.name}`, {type: `${tbh}`});
+      message.react("\u2705")
+} else if(message.author.id === "245877990938902529") {
+bot.user.setActivity(`${bot.user.presence.game.name}`, {type: `${tbh}`});
+      message.react("\u2705")
+    }
+
+}
+
+module.exports.help = {
+    name: "setactivity"
+}
