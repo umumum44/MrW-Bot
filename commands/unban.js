008c4522e801f4867aef7bb955e4de1949a39765
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 11:27:23 -0500

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -2,15 +2,14 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let name = `${args[0]}`
   if(message.member.hasPermission("BAN_MEMBERS")) {
-    let tounban = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!tounban) {
-      let aarray = await message.guild.fetchBans()
-      let marray = aarray.filter(m => RegExp(name, "gi").test(m.displayName))
-      tounban = marray.first()
-    }
-    if(!tounban) return message.reply("Couldn't find this user.")
-      message.guild.unban(tounban)
+    
+    if(!name) return message.reply("You must provide a user ID!")
+      try {
+    message.guild.unban(name)
       message.react("\u2705")
+         } catch (e) {
+        return message.channel.send("Couldn't find this user!")            
+         }
 
 }
 }
