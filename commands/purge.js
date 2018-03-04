207e85b638f4d3460ca484bcf1a42315aac6b033
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 11:55:59 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -4,6 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
 if(message.member.hasPermission("MANAGE_MESSAGES")) {
   let num = parseInt(args[0])
+  if(!num) return message.channel.send("You must provide the number of messages to delete!")
   if(num > 100) return message.channel.send("You can only purge 100 messages at a time!")
 message.channel.bulkDelete(args[0])
 .then(messages => message.channel.send(`Deleted ${messages.size} messages!`))
