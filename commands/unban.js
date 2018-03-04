7815778b42cdea3b6fce40ad7baaed139433eb72
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 10:45:58 -0500

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message, args) => {
   if(message.member.hasPermission("BAN_MEMBERS")) {
     let tounban = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!tounban) {
-      aarray = message.guild.fetchBans()
-      marray = marray.filter(m => RegExp(name, "gi").test(m.displayName))
+      let aarray = message.guild.fetchBans()
+      let marray = marray.filter(m => RegExp(name, "gi").test(m.displayName))
       tounban = marray.first()
     }
     if(!tounban) return message.reply("Couldn't find this user.")
