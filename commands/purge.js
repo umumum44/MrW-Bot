48eadebc5d93f2d84ada95ea45b6c6f1174dbe6c
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 11:43:15 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 if(message.member.hasPermission("MANAGE_MESSAGES")) {
 message.channel.bulkDelete(args[0])
 .then(messages => message.channel.send(`Deleted ${messages.size} messages!`))
-  .catch(console.error);
+  .catch(message.channel.send(console.error));
 } else return message.channel.send(`${message.author}, you do not have permission to purge messages!`)
 
 }
