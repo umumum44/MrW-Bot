cac6c2a1086c44dde24ffbf3325372093041528f
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 13:18:21 -0500

    Create 8ball.js

---
+++ commands/8ball.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  var responses = ["no.","highly unlikely.","very doubtful.","you wish.","don't count on it", "probably.", "most definitely", "yes.", "highly likely.", "of course.",
+                  "ask again later.", "outlook not so good, retry.", "ask again later."];
+  var randomchoice = Math.floor(Math.random() * choices.length);
+  message.reply("The 8ball says..."+responses[randomchoice]);
+}
+module.exports.help = {
+  name: "8ball";
+}
