51ceffa122ba5885156ca069fd9a00ccc8fda4fe
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 11:00:12 -0500

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let name = `${args[0]}`
   if(message.member.hasPermission("BAN_MEMBERS")) {
-      if (!name) return message.channel.send("You must supply a username!")
     let tounban = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!tounban) {
       let aarray = await message.guild.fetchBans()
