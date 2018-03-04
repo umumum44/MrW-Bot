f49cb41f914469e0ed2796617b59e116b389f945
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 15:06:29 -0500

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  name = args[0]
   let target = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if (!target) {
     marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName));
