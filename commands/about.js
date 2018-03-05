19c769355623910b8a351033c02a115de48643d7
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 21:34:50 -0500

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -1,28 +1,27 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-		let name = `${args[0]}`;
-let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-if(!user) {
-	marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName));
-	user = marray.first();
+	let name = `${args[0]}`;
+	let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+	if(!user) {
+		marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName));
+		user = marray.first();
 	}
-if(!user) return message.channel.send("Couldn't find this user.")
-
-let usericon = user.user.avatarURL
-    let ab = new Discord.RichEmbed()
-    .setTitle("User Information")
-    .setColor("#000080")
-    .setThumbnail(usericon)
-    .addField("Username", user.user.username)
-    .addField("Discriminator", user.user.discriminator)
-    .addField("User ID", user.user.id)
-    .addField("Joined At", user.joinedAt)
-    .addField("Created At", user.user.createdAt);
-    
-
-    return message.channel.send(ab);
-  }
+	if(!user) return message.channel.send("Couldn't find this user.")
+	
+	let usericon = user.user.avatarURL
+	let ab = new Discord.RichEmbed()
+	.setTitle("User Information")
+	.setColor("#000080")
+	.setThumbnail(usericon)
+	.addField("Username", user.user.username)
+	.addField("Discriminator", user.user.discriminator)
+	.addField("User ID", user.user.id)
+	.addField("Joined At", user.joinedAt)
+	.addField("Registered At", user.user.createdAt);
+	
+	return message.channel.send(ab);
+}
 
 module.exports.help = {
 	name: "about"
-	}
+}
