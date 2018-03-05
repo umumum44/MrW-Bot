eff6d3d6f2e8ef929ee421dc3a855d50355d00f0
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 21:00:58 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 	let name = `${args[0]}`
+	let reason = message.content.substr(7+args[0].length);
 	if(message.member.hasPermission("KICK_MEMBERS")) {
 		let kickeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 		if(!kickeduser) {
@@ -8,8 +9,10 @@ module.exports.run = async (bot, message, args) => {
 			kickeduser = marray.first();
 		}
 		if(!kickeduser) return message.reply("Couldn't find this user!")
-		if(kickeduser.hasPermission("KICK_MEMBERS")) return message.channel.send(`${message.author}, this member cannot be kicked!`);
-		message.guild.member(kickeduser).kick();
+		//if(kickeduser.hasPermission("KICK_MEMBERS")) return message.channel.send(`${message.author}, this member cannot be kicked!`);
+		//message.guild.member(kickeduser).kick();
+		buser.send(`You were kicked in ${message.guild.name} for \`${reason}\` by ${message.author.username}`);
+		
 		message.react("\u2705");
 	} else return message.channel.send(`${message.author}, you do not have permission to kick members!`);
 	return;
