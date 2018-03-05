29dc28190fe5ab2baf6dba60af848e16d35e2854
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 20:28:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,55 +6,34 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
-
-  if(err) console.log(err);
-
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-  jsfile.forEach((f,i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
+	if(err) console.log(err);
+	let jsfile = files.filter(f => f.split(".").pop() === "js")
+	if(jsfile.length <= 0){
+		console.log("Couldn't find commands.");
+		return;
+	}
+	jsfile.forEach((f,i) =>{
+		let props = require(`./commands/${f}`);
+		console.log(`${f} loaded!`);
+		bot.commands.set(props.help.name, props);
+	});
 });
+
 bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity("Games", {type: "PLAYING"});
+	console.log(`${bot.user.username} is online!`);
+	bot.user.setActivity("Games", {type: "PLAYING"});
 });
-
 bot.on("message", async message => {
-  //if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-
-if((message.content.endsWith("messages that were not over two weeks old!")) && (message.author.bot)) {
-   message.delete(5000)
-}
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-  
-if(!message.content.startsWith(botconfig.prefix)) return;
-let commandfile = bot.commands.get(cmd.slice(prefix.length));
-return commandfile.run(bot, message, args);
-
-var pgClient = new pg.Client(botconfig.db);
-pgClient.connect();
-  const query = {
-  text: 'INSERT INTO users(id, warnings) VALUES($1, $2)',
-  values: ['123456', 'ethanlaj'],
-}
-let cjthedj = await pgClient.query("SELECT warnings FROM users WHERE id = $1 ORDER BY ID", id);
-console.log(require("util").inspect(cjthedj))
-
-  
-  
-  
+	if(message.channel.type === "dm") return;
+	if((message.content.endsWith("messages that were not over two weeks old!")) && (message.author.bot)) {
+		message.delete(5000)
+	}
+	let prefix = botconfig.prefix;
+	let messageArray = message.content.split(" ");
+	let cmd = messageArray[0];
+	let args = messageArray.slice(1);
+	if(!message.content.startsWith(botconfig.prefix)) return;
+	let commandfile = bot.commands.get(cmd.slice(prefix.length));
+	return commandfile.run(bot, message, args);
 })
-
 bot.login(botconfig.token);
