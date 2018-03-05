b232509163d7610f7266115fb9cbc6f5567625f0
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 20:08:56 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,21 +1,20 @@
 const Discord = require("discord.js");
-
 module.exports.run = async (bot, message, args) => {
-  let name = `${args[0]}`
-if(message.member.hasPermission("BAN_MEMBERS")) {
-  let buser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!buser) {
-    marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName))
-    buser = marray.first()
-  }
-  if(!buser) return message.reply("Couldn't find this user!")
-    if(buser.hasPermission("BAN_MEMBERS")) return message.channel.send(`${message.author}, this member cannot be banned!`)
-    message.guild.member(buser).ban()
-    message.react("\u2705")
-    } else return message.channel.send(`${message.author}, you do not have permission to kick members!`)
-return;
+	let name = `${args[0]}`;
+	if(message.member.hasPermission("BAN_MEMBERS")) {
+		let buser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+		if(!buser) {
+			marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName));
+			buser = marray.first();
+		}
+		if(!buser) return message.reply("Couldn't find this user!")
+		if(buser.hasPermission("BAN_MEMBERS")) return message.channel.send(`${message.author}, this member cannot be banned!`);
+		message.guild.member(buser).ban();
+		message.react("\u2705");
+	} else return message.channel.send(`${message.author}, you do not have permission to kick members!`);
+	return;
 }
 
 module.exports.help = {
-    name: "ban"
+	name: "ban"
 }
