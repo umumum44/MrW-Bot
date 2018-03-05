97ae1dcd8ed53e25eda70ff6fabdd1450de25ed7
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 20:25:06 -0500

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,22 +1,21 @@
 const Discord = require("discord.js");
 const ms = require("ms");
 module.exports.run = async (bot, message, args) => {
-  let name = `${args[0]}`
-  if(message.member.hasPermission("KICK_MEMBERS")) {
-    let tounmute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!tounmute) {
-      marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName))
-      tounmute = marray.first()
-    }
-    if(!tounmute) return message.reply("Couldn't find this user.")
-    let muterole = message.guild.roles.find(`name`, "Muted");
-    if(tounmute.roles.has(muterole.id)) {
-      tounmute.removeRole(muterole)
-      message.react("\u2705")
-    } else return message.reply("This user is not muted!")
-
-}
+        let name = `${args[0]}`
+        if(message.member.hasPermission("KICK_MEMBERS")) {
+                let tounmute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+                if(!tounmute) {
+                        marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName))
+                        tounmute = marray.first()
+                }
+                if(!tounmute) return message.reply("Couldn't find this user.")
+                let muterole = message.guild.roles.find(`name`, "Muted");
+                if(tounmute.roles.has(muterole.id)) {
+                        tounmute.removeRole(muterole)
+                        message.react("\u2705")
+                } else return message.reply("This user is not muted!")
+        }
 }
 module.exports.help = {
-    name: "unmute"
+        name: "unmute"
 }
