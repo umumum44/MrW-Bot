5aea3a77943a761cfea2d637765df0cb7fe8e1b3
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 21:44:42 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,23 +1,22 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
+	let helper = new Discord.RichEmbed()
+	.setTitle("Commands")
+	.addField("8Ball", "An 8-Ball simulator")
+	.addField("About", "Gets information about a server member")
+	.addField("Ban", "Bans a server member")
+	.addField("Kick", "Kicks a server member")
+	.addField("Mute", "Mutes a server member")
+	.addField("Ping", "Responds with pong!")
+	.addField("Purge", "Deletes a specified amount of messages")
+	.addField("Report", "Starts a prompt for reporting bugs/glitches in games")
+	.addField("Setactivity", "Sets the bot's activity \(Owner-Only\)")
+	.addField("Setstatus", "Sets the bot's status \(Owner-Only\)")
+	.addField("Unmute", "Unmutes a server member");
 
-  let helper = new Discord.RichEmbed()
-  .setTitle("Commands")
-  .addField("8ball", "An 8-Ball simulator")
-  .addField("about", "Gets information about a server member")
-  .addField("ban", "Bans a server member")
-  .addField("kick", "Kicks a server member")
-  .addField("mute", "Mutes a server member")
-  .addField("ping", "Responds with pong!")
-  .addField("purge", "Deletes a specified amount of messages")
-  .addField("report", "Starts a prompt for reporting bugs/glitches in games")
-  .addField("setactivity", "Sets the bot's activity \(Owner-Only\)")
-  .addField("setstatus", "Sets the bot's status \(Owner-Only\)")
-  .addField("unmute", "Unmutes a server member");
-
-  message.react("\u2705")
-  message.author.send(helper)
+	message.react("\u2705")
+	message.author.send(helper)
 }
 module.exports.help = {
-    name: "help"
+	name: "help"
 }
