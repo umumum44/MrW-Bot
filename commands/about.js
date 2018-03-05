4d958cde37125ff2a9de7c2171252635d505f589
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 21:28:11 -0500

    Create about.js

---
+++ commands/about.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(!user) {
+	marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName));
+	user = marray.first();
+	}
+if(!user) return message.channel.send("Couldn't find this user.")
+
+let usericon = user.user.avatarURL
+    let ab = new Discord.RichEmbed()
+    .setTitle("User Information")
+    .setColor("#000080")
+    .setThumbnail(usericon)
+    .addField("Username", user.user.username)
+    .addField("Discriminator", user.user.discriminator)
+    .addField("User ID", user.user.id)
+    .addField("Joined At", user.joinedAt);
+    .addField("Created At", user.user.createdAt);
+    
+
+    return message.channel.send(ab);
+  }
+}
+
+module.exports.help = {
+	name: "about"
+	}
