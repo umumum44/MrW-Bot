582e37815103d5e5c596c6a4a1ee28c61ff7075e
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 21:30:31 -0500

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
+		let name = `${args[0]}`;
 let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 if(!user) {
 	marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName));
