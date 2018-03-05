22a0ef82eeb1ce4e33c6a6d07037de15388ccae2
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 21:06:21 -0500

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -33,6 +33,9 @@ module.exports.run = async (bot, message, args) => {
 			let mutetime = args[1];
 			if(!mutetime) return message.reply("You must specify a time!");
 			await(tomute.addRole(muterole.id));
+			let reason = message.content.substr(6+args[0].length);
+			if(!reason) reason = "No reason specified";
+			tomute.send(`You were banned in ${message.guild.name} for \`${reason}\` for \`${mutetime}\` by ${message.author.username}`);
 			message.react("\u2705");
 			setTimeout(function(){
 				tomute.removeRole(muterole.id);
