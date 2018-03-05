1664bd2a1aa5d679c1cdb56a7d6e6922bc2344a4
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-05 07:48:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.on("ready", async () => {
 	console.log(`${bot.user.username} is online!`);
-	bot.user.setActivity(".help", {type: "PLAYING"});
+	bot.user.setActivity("Games", {type: "PLAYING"});
 });
 bot.on("message", async message => {
 	if(message.channel.type === "dm") return;
