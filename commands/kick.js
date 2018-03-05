6692ff6c9c1b6e94411d97ac9e323607f680309c
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 21:04:01 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -10,8 +10,8 @@ module.exports.run = async (bot, message, args) => {
 			kickeduser = marray.first();
 		}
 		if(!kickeduser) return message.reply("Couldn't find this user!")
-		//if(kickeduser.hasPermission("KICK_MEMBERS")) return message.channel.send(`${message.author}, this member cannot be kicked!`);
-		//message.guild.member(kickeduser).kick();
+		if(kickeduser.hasPermission("KICK_MEMBERS")) return message.channel.send(`${message.author}, this member cannot be kicked!`);
+		message.guild.member(kickeduser).kick();
 		kickeduser.send(`You were kicked in ${message.guild.name} for \`${reason}\` by ${message.author.username}`);
 		
 		message.react("\u2705");
