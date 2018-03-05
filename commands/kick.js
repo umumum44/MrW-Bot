c4d8d2c760b387b4430f40ba1ce694a2dfc55b45
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 20:10:54 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,21 +1,20 @@
 const Discord = require("discord.js");
-
 module.exports.run = async (bot, message, args) => {
-  let name = `${args[0]}`
-if(message.member.hasPermission("KICK_MEMBERS")) {
-  let kickeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!kickeduser) {
-    marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName))
-    kickeduser = marray.first()
-  }
-  if(!kickeduser) return message.reply("Couldn't find this user!")
-    if(kickeduser.hasPermission("KICK_MEMBERS")) return message.channel.send(`${message.author}, this member cannot be kicked!`)
-    message.guild.member(kickeduser).kick()
-    message.react("\u2705")
-    } else return message.channel.send(`${message.author}, you do not have permission to kick members!`)
-return;
+	let name = `${args[0]}`
+	if(message.member.hasPermission("KICK_MEMBERS")) {
+		let kickeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+		if(!kickeduser) {
+			marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName));
+			kickeduser = marray.first();
+		}
+		if(!kickeduser) return message.reply("Couldn't find this user!")
+		if(kickeduser.hasPermission("KICK_MEMBERS")) return message.channel.send(`${message.author}, this member cannot be kicked!`);
+		message.guild.member(kickeduser).kick();
+		message.react("\u2705");
+	} else return message.channel.send(`${message.author}, you do not have permission to kick members!`);
+	return;
 }
 
 module.exports.help = {
-    name: "kick"
+	name: "kick"
 }
