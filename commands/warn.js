fd7dca24c6878a3870cdeee4a6597c4abc030948
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-06 21:55:39 -0500

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -9,38 +9,38 @@ module.exports.run = async (bot, message, args) => {
   if (target) {
     if(message.member.hasPermission("KICK_MEMBERS") || message.member.hasPermission("ADMINISTRATOR") || message.member.hasPermission("BAN_MEMBERS")) {
       if (message.member.highestRole.position < target.highestRole.position) return message.reply("You are not high enough in this guild's hierarchy to warn this user.");
-      var reason;
       if (args[1] == null) {
         // checks if there is a reason
-        reason = "No reason specified";
+        var reason = "No reason specified";
       } else {
-        reason = message.content.substr(7+args[0].length);
+        var reason = message.content.substr(7+args[0].length);
         // takes out the user mention/id/name and command to result in everything else
       }
       message.delete();
       target.send(`You were warned in ${message.guild} for `+`\`${reason}\``+` by ${message.author.username}`);
       var dbchannel = bot.channels.get("420706931011747842");
-      var db = dbchannel.fetchMessages()
       var warningschannel = bot.channels.get("420708216188567552");
-      db.forEach(function(msg) {
-        var msgcontent = msg.content
-        let messageArray = msgcontent.split(" ");
-        let args = messageArray.slice(1);
-        if (msgcontent.startsWith(`${message.guild.id} ${target.id}`)) {
-          var amount = args[2];
-          msg.edit(`${message.guild.id} ${target.id} ${amount+1}`);
-          amount = amount+1;
-          warningschannel.send(`${message.guild.id} ${target.id} ${reason}`);
-          if (amount == 3) {
-            target.kick("Reaching 3 warnings");
-          } else if (amount == 6) {
-            target.kick("Reaching 6 warnings");
-          } else if (amount == 9) {
-            target.ban("Reaching 9 warnings");
+      dbchannel.fetchMessages().then(messages => {
+        messages.forEach(msg => {
+          var msgcontent = msg.content
+          let messageArray = msgcontent.split(" ");
+          let args = messageArray.slice(1);
+          if (msgcontent.startsWith(`${message.guild.id} ${target.id}`)) {
+            var amount = args[2];
+            msg.edit(`${message.guild.id} ${target.id} ${amount+1}`);
+            amount = amount+1;
+            warningschannel.send(`${message.guild.id} ${target.id} ${reason}`)
+            if (amount == 3) {
+              target.kick("Reaching 3 warnings");
+            } else if (amount == 6) {
+              target.kick("Reaching 6 warnings");
+            } else if (amount == 9) {
+              target.ban("Reaching 9 warnings");
+            }
+          } else {
+            dbchannel.send(`${message.guild.id} ${target.id} 1`);
           }
-        } else {
-          dbchannel.send(`${message.guild.id} ${target.id} 1`);
-        }
+        });
       });
     } else {
       message.reply("Insufficent permissions.");
