0171e3410caa9a101bca4c9b94a1c8d667cee1d0
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-11 16:31:27 -0400

    Update rps.js

--- commands/rps.js
+++ commands/rps.js
@@ -5,20 +5,20 @@ if(!choice) return message.reply("You did not provide your choice!")
 	var responses = ["rock", "paper", "scissors"];
 	var randomchoice = Math.floor(Math.random() * responses.length);
   if(randomchoice.toLowerCase() === "rock" && choice.toLowerCase() === "paper") {
-	message.reply(`I chose **${randomchoice}.** You chose paper. You win!);
+	message.reply(`I chose **${randomchoice}.** You chose paper. You win!`);
   } else if(randomchoice.toLowerCase() === "rock" && choice.toLowerCase() === "scissors") {
-	message.reply(`I chose **${randomchoice}.** You chose scissors. You win!);
+	message.reply(`I chose **${randomchoice}.** You chose scissors. You win!`);
   } else if(randomchoice.toLowerCase() === "paper" && choice.toLowerCase() === "rock") {
-	message.reply(`I chose **${randomchoice}.** You chose scissors. You win!);
+	message.reply(`I chose **${randomchoice}.** You chose scissors. You win!`);
   } else if(randomchoice.toLowerCase() === "paper" && choice.toLowerCase() === "scissors") {
-	message.reply(`I chose **${randomchoice}.** You chose scissors. You win!);
+	message.reply(`I chose **${randomchoice}.** You chose scissors. You win!`);
   } else if(randomchoice.toLowerCase() === "scissors" && choice.toLowerCase() === "rock") {
-	message.reply(`I chose **${randomchoice}.** You chose scissors. You win!);
+	message.reply(`I chose **${randomchoice}.** You chose scissors. You win!`);
   } else if(randomchoice.toLowerCase() === "scissors" && choice.toLowerCase() === "paper") {
-	message.reply(`I chose **${randomchoice}.** You chose scissors. You win!);
+	message.reply(`I chose **${randomchoice}.** You chose scissors. You win!`);
   } else if(randomchoice.toLowerCase() === choice.toLowerCase()) {
   let l = choice.toLowerCase()
-	message.reply(`I chose **${randomchoice}.** You chose ${l}. It's a tie!);
+	message.reply(`I chose **${randomchoice}.** You chose ${l}. It's a tie!`);
   } else return message.reply("You did not provide a valid choice!\nValid Choices: Rock, Paper, Scissors")
   
 }
