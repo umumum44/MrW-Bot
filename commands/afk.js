73bd68efdb0a952c3435343634c1e7a8958089b0
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-10 20:39:47 -0500

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -1,9 +1,9 @@
 module.exports.run = async (bot, message, args) => {
   let afkmsg = args.join(" ")
   let channel = bot.channels.find(`id`, "422201325623836682")
-  channel.send(`${message.author.id},${afkmsg}, ${message.author.username}#${message.author.discriminator}`)
+  channel.send(`${message.author.id}, ${afkmsg}, ${message.author.username}#${message.author.discriminator}`)
 
-message.reply(`You are now AFK!: \`${afkmsg}\'\nTo become un-AFK, just talk again!`)
+message.reply(`You are now AFK!: \`${afkmsg}\`\nTo become un-AFK, just talk again!`)
 
 }
 module.exports.help = {
