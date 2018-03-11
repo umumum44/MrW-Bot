e018dd8bd88c6be04ce7605f2512499de75bf2cc
Yikey <gtc122004@gmail.com>
2018-03-11 13:26:23 -0400

    Create warn.js

---
+++ commands/warn.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  let target = message.guild.member(message.mentions.users.first());
+  if (target) {
+    if(message.member.hasPermission("KICK_MEMBERS") || message.member.hasPermission("ADMINISTRATOR") || message.member.hasPermission("BAN_MEMBERS")) {
+      if (message.member.highestRole.position < target.highestRole.position) return message.reply("You are not high enough in this guild's hierarchy to warn this user.");
+      if (args[1] == null) {
+        // checks if there is a reason
+        var reason = "No reason specified";
+      } else {
+        var reason = message.content.substr(7+args[0].length);
+        // takes out the user mention/id/name and command to result in everything else
+      }
+      target.send(`You were warned in \`${message.guild.name}\` for \`${reason}\` by \`${message.author.username}#${message.author.discriminator}\``)
+      var dbguild = bot.guilds.get("417149156193337344");
+      var dbchannel = dbguild.channels.find("name", "warn-database")
+      var olo = await dbchannel.fetchMessages({ limit: 100 });
+      var msgcount = olo.size;
+      if (msgcount == "100") {
+        await dbchannel.setName("archived-warn-database");
+        // Create a new category channel with permission overwrites
+        dbguild.createChannel('warn-database', 'text', [{
+          id: dbguild.id,
+          deny: ['READ_MESSAGES'],
+          allow: []
+        }]).then(() => {
+           dbchannel = dbguild.channels.find("name", "warn-database");
+           dbchannel.setParent("422122104499208214");
+           dbchannel.send(`${message.guild.id} ${target.id} ${reason}`);
+        });
+      } else {
+        dbchannel.send(`${message.guild.id} ${target.id} ${reason}`)
+      }
+    } else {
+      message.reply("Insufficent permissions.");
+    }
+  } else {
+    message.reply("Please **mention** a valid user.");
+  }
+}
+module.exports.help = {
+  name: "warn"
+}
