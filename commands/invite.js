3235258c795bcdb6fd2d4907758175155493e20b
Yikey <gtc122004@gmail.com>
2018-03-12 23:00:09 -0400

    indent fix 1

--- commands/invite.js
+++ commands/invite.js
@@ -1,15 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-
-
-message.reply("**Invite me here!:** https://discordapp.com/oauth2/authorize?client_id=419881218784493588&permissions=347201&scope=bot")
-
-
-
-
-
+      message.reply("**Invite me here!:** https://discordapp.com/oauth2/authorize?client_id=419881218784493588&permissions=347201&scope=bot")
 }
 module.exports.help = {
       name: "invite"
-  }
+}
