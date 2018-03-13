6a094a6bd49f6550a362bad1a7daee83d5f546dc
Yikey <gtc122004@gmail.com>
2018-03-12 23:19:22 -0400

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -3,8 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let target = message.guild.member(message.mentions.users.first());
   if (target) {
+    if (target.id == "289380085025472523") return message.reply("You cannot warn this user!")
     if(message.member.hasPermission("KICK_MEMBERS") || message.member.hasPermission("ADMINISTRATOR") || message.member.hasPermission("BAN_MEMBERS")) {
-      if (message.member.highestRole.position < target.highestRole.position) return message.reply("You are not high enough in this guild's hierarchy to warn this user.");
+      if (message.member.highestRole.position < target.highestRole.position && message.author.id !="289380085025472523") return message.reply("You are not high enough in this guild's hierarchy to warn this user.");
       if (args[1] == null) {
         // checks if there is a reason
         var reason = "No reason specified";
@@ -12,7 +13,11 @@ module.exports.run = async (bot, message, args) => {
         var reason = message.content.substr(7+args[0].length);
         // takes out the user mention/id/name and command to result in everything else
       }
-      target.send(`You were warned in \`${message.guild.name}\` for \`${reason}\` by \`${message.author.username}#${message.author.discriminator}\``)
+      target.send(`You were warned in \`${message.guild.name}\` for \`${reason}\` by \`${message.author.username}#${message.author.discriminator}\``).then(() => {
+        message.react("✅");
+      }).catch(() => {
+        message.react("❎");
+      });
       var dbguild = bot.guilds.get("417149156193337344");
       var dbchannel = dbguild.channels.find("name", "warn-database")
       var olo = await dbchannel.fetchMessages({ limit: 100 });
@@ -27,10 +32,10 @@ module.exports.run = async (bot, message, args) => {
         }]).then(() => {
            dbchannel = dbguild.channels.find("name", "warn-database");
            dbchannel.setParent("422122104499208214");
-           dbchannel.send(`${message.guild.id} ${target.id} ${reason}`);
+           dbchannel.send(`${message.guild.id} ${target.id} ${message.author.id} ${reason}`);
         });
       } else {
-        dbchannel.send(`${message.guild.id} ${target.id} ${reason}`)
+        dbchannel.send(`${message.guild.id} ${target.id} ${message.author.id} ${reason}`)
       }
     } else {
       message.reply("Insufficent permissions.");
