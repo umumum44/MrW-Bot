f1818e0afbd96eb4886d2b5d1fd4b3ba153f8db7
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-15 21:15:59 -0400

    Update unblacklist.js

--- commands/unblacklist.js
+++ commands/unblacklist.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-	if(message.author.id === "399975738008141824" || message.author.id === "303683211790254080" || message.author.id === "245877990938902529" || message.author.id === "289380085025472523" {
+	if(message.author.id === "399975738008141824" || message.author.id === "303683211790254080" || message.author.id === "245877990938902529" || message.author.id === "289380085025472523") {
 
       let channel = bot.channels.find(`id`, "424006591411519499")
       let pingeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
