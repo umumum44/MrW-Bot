5e7705ce418cf8db1cb0210055ad023dfdcf9a5b
Yikey <gtc122004@gmail.com>
2018-03-17 14:00:50 -0400

    Update rps.js

--- commands/rps.js
+++ commands/rps.js
@@ -1,25 +1,16 @@
 const Discord = require("discord.js");
+
 module.exports.run = async (bot, message, args) => {
-	let choice = args[0]
-	if(!choice) return message.reply("You did not provide your choice!")
+	let choice = args[0];
 	var responses = ["rock", "paper", "scissors"];
+	if (!choice) return message.reply("You did not provide your choice!")
+	if (!responses.includes(choice.toLowerCase())) return message.reply("Please specify one of the following as a choice: `rock`, `paper` or `scissors`.");
+	var responsesop = ["paper", "scissors", "rock"];
 	var randomchoice = Math.floor(Math.random() * responses.length);
-	if(responses[randomchoice].toLowerCase() === "rock" && choice.toLowerCase() === "paper") {
-		message.reply(`I chose **${responses[randomchoice]}.** You chose **paper**. You win!`);
-	} else if(responses[randomchoice].toLowerCase() === "rock" && choice.toLowerCase() === "scissors") {
-		message.reply(`I chose **${responses[randomchoice]}.** You chose **scissors**. I win!`);
-	} else if(responses[randomchoice].toLowerCase() === "paper" && choice.toLowerCase() === "rock") {
-		message.reply(`I chose **${responses[randomchoice]}.** You chose **rock**. I win!`);
-	} else if(responses[randomchoice].toLowerCase() === "paper" && choice.toLowerCase() === "scissors") {
-		message.reply(`I chose **${responses[randomchoice]}.** You chose **scissors**. You win!`);
-	} else if(responses[randomchoice].toLowerCase() === "scissors" && choice.toLowerCase() === "rock") {
-		message.reply(`I chose **${responses[randomchoice]}.** You chose **rock**. You win!`);
-	} else if(responses[randomchoice].toLowerCase() === "scissors" && choice.toLowerCase() === "paper") {
-		message.reply(`I chose **${responses[randomchoice]}.** You chose **paper**. I win!`);
-	} else if(responses[randomchoice].toLowerCase() === choice.toLowerCase()) {
-		let l = choice.toLowerCase()
-		message.reply(`I chose **${responses[randomchoice]}.** You chose **${l}**. It's a tie!`);
- 	} else return message.reply("You did not provide a valid choice!\n**Valid Choices:** Rock, Paper, Scissors")
+	if (choice.toLowerCase() == responses[randomchoice]) return message.reply(`You choice \`${choice}\`, I chose \`${responses[randomchoice]}\`! It's a draw!`);
+	if (responsesop.indexOf(choice) == randomchoice) return message.reply(`You choice \`${choice}\`, I chose \`${responses[randomchoice]}\`! You win!`);
+	if (responsesop.indexOf(choice) != randomchoice) return message.reply(`You choice \`${choice}\`, I chose \`${responses[randomchoice]}\`! You lose!`);
+
 }
 module.exports.help = {
 	name: "rps"
