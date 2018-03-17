edd950e311332e61e07f7ef1718218463536261f
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-17 09:42:09 -0400

    Update countstatus.js

--- commands/countstatus.js
+++ commands/countstatus.js
@@ -1,23 +1,17 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-
-if((message.author.id === "291367352476631040") && (bot.counter === false)) {
+let guild = bot.guilds.find(`id`, "410400562232819723")
+let member = await guild.fetchMember(message.author.id)
+if(!member) return;
+if (member.roles.get("410608939139334184") //coowner
+ || member.roles.get("410481036162760722")) { //owner 
+if (bot.counter === false) {
 bot.user.setActivity(`${bot.guilds.size} servers`, {type: "WATCHING"});
       message.react("\u2705")
       bot.counter = true
-} else if((message.author.id === "245877990938902529") && (bot.counter === false)) {
-bot.user.setActivity(`${bot.guilds.size} servers`, {type: "WATCHING"});
-      message.react("\u2705")
-      bot.counter = true
-    } else if ((message.author.id === "291367352476631040") && (bot.counter === true)) {
+    } else if (bot.counter === true) {
             bot.user.setActivity("for !help", {type: "WATCHING"});
                 message.react("\u2705")
-
-      bot.counter = false
-
-} else if ((message.author.id === "245877990938902529") && (bot.counter === true)) {
-            bot.user.setActivity("for !help", {type: "WATCHING"});
-            message.react("\u2705")
 
       bot.counter = false
 }
