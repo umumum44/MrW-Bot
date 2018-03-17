6176a968057f172a9a49acaa03c3f0a5a555af84
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-17 09:42:54 -0400

    Update setactivity.js

--- commands/setactivity.js
+++ commands/setactivity.js
@@ -1,10 +1,14 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
+        let guild = bot.guilds.find(`id`, "410400562232819723")
+let member = await guild.fetchMember(message.author.id)
+if(!member) return;
+if  (member.roles.get("410608939139334184") //coowner
+ || member.roles.get("410481036162760722")) { //owner 
         let tbh = args.join(" ").toUpperCase();
-        if(message.author.id === "399975738008141824") {
                 bot.user.setActivity(`${bot.user.presence.game.name}`, {type: `${tbh}`});
                 message.react("\u2705");
-        }
+}
 }
 
 module.exports.help = {
