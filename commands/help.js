12d883e59b214b58cf356f31434c7395ede167a2
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-17 21:25:27 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 	let helper = new Discord.RichEmbed()
 	.setTitle("Commands")
-	.setDescription("**Bot Prefix: !!**")
+	.setDescription("\`Bot Prefix:\` !!")
 	.addField("Fun", "\`!!8ball\` - An 8-Ball Simulation\n\`!!rps\` - Plays a game of rock-paper-scissors")
 	.addField("Information", "\`!!about\` - Gets information about a server member\n\`!!getinfo\` - Sends information about a discord user's linked roblox account\n\`!!server\` - Sends the link to the support server\n\`!!invite\` - Sends the link to invite me")
 	.addField("Moderation", "\`!!ban\` - Bans a server member\n\`!!kick\` - Kicks a server member\n\`!!mute\` - Mutes a server member\n\`!!unmute\` - Unmutes a server member\n\`!!purge\` - Deletes a specified amount of messages\n\`!!warn\` - Warns a user\n\`!!warnings\` - Shows the warnings for a user")
