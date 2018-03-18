f319959c1b077b52b10e956651e43a06ea378511
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-17 21:22:38 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args) => {
 	let helper = new Discord.RichEmbed()
 	.setTitle("Commands")
 	.setDescription("**Bot Prefix: !!**")
-	.addField("Fun", "!!8ball - An 8-Ball Simulation\n!!rps - Plays a game of rock-paper-scissors")
+	.addField("Fun", "\`!!8ball\` - An 8-Ball Simulation\n\`!!rps\` - Plays a game of rock-paper-scissors")
 	.addField("Information", "\`!!about\` - Gets information about a server member\n\`!!getinfo\` - Sends information about a discord user's linked roblox account\n\`!!server\` - Sends the link to the support server\n\`!!invite\` - Sends the link to invite me")
 	.addField("Moderation", "\`!!ban\` - Bans a server member\n\`!!kick\` - Kicks a server member\n\`!!mute\` - Mutes a server member\n\`!!unmute\` - Unmutes a server member\n\`!!purge\` - Deletes a specified amount of messages\n\`!!warn\` - Warns a user\n\`!!warnings\` - Shows the warnings for a user")
 	.addField("Miscellaneous", "\`!!afk\` - Sets a message to be sent whenever you are pinged\n\`!!report\` - Starts a prompt for reporting bugs/glitches in games\n\`!!ping\` - Responds with the speed of the bot\n\`!!verify\` - Verifies your roblox account with your discord account")
