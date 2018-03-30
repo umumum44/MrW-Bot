0b98addafb6a569097cc98eec8a5555482214829
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-30 16:12:49 -0400

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -6,9 +6,9 @@ if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply("You don'
 if(!args[0]) return message.reply("Please provide the new prefix!")
 let prefix = args[0]
 let dbguild = bot.guilds.find(`id`, "417149156193337344");
-let channel = dbguild.channels.find(`name`, "prefix-database")
+let channel = dbguild.channels.find(`name`, "wprefix-database")
 let messages = await channel.fetchMessages({ limit: 100 })
-let channels = dbguild.channels.filter(m => RegExp("prefix-database", "gi").test(m.name));
+let channels = dbguild.channels.filter(m => RegExp("wprefix-database", "gi").test(m.name));
 channels.forEach(chl => {
   chl.fetchMessages({ limit: 100 }).then(msgs => {
       msgs.forEach(msg => {
