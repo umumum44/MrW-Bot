977ed5f7f313e185016d21f86de331119a6a1a90
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-02 17:24:51 -0400

    Create news.js

---
+++ commands/news.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+
+let channel = bot.channels.find(`id`, "430477691154595852")
+let editor = await channel.fetchMessage("429100390206668800")
+let thing = new Discord.RichEmbed()
+	.setTitle("News")
+ .setColor("#FF0000")
+ .setDescription(editor.content)
+.setFooter(`Requested by ${message.author.tag}`)
+ await message.channel.send(thing)
+
+}
+
+
+
+module.exports.help = {
+	name: "news"
+}
