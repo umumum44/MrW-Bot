7037c760f9f6841c8f910561ec7c6bfe987a2146
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-03 07:50:22 -0400

    Update softban.js

--- commands/softban.js
+++ commands/softban.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix) => {
+	var reason;
 	let name = `${args[0]}`;
 	if(!reason) {
 		reason = "No reason specified";
@@ -9,31 +10,25 @@ module.exports.run = async (bot, message, args, prefix) => {
 	if(message.member.hasPermission("BAN_MEMBERS")) {
 		let buser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 		if(!buser) {
-			marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName));
+			let marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName));
 			buser = marray.first();
 		}
 		if (!buser) return message.reply("Couldn't find this user!");
 		if (message.member.highestRole.position <= buser.highestRole.position) return message.reply("This user is too high up in this guilds' hierarchy to be banned by you!");
 		try {
-		buser.send(`You were soft-banned in ${message.guild.name} for \`${reason}\` by ${message.author.username}`).then(() => {
-			message.guild.member(buser).ban(7).then(() => {
-        message.guild.unban(buser.id);
-      	message.react("✅");
-    	}).catch(() => {
-				message.react("❎");
-    	});
-		}).catch(() => {
-			message.guild.member(buser).ban(7).then(() => {
-        message.guild.unban(buser.id);
-      	message.react("✅");
-      }).catch(() => {
-				message.react("❎");
-      });
-		});
+		buser.send(`You were soft-banned in ${message.guild.name} for \`${reason}\` by ${message.author.username}`)
 		}
 		catch (e) {
 			message.reply("Couldn't DM this user to tell them they were softbanned.")
 		}
+			message.guild.member(buser).ban(7)
+        message.guild.unban(buser.id);
+      	message.react("✅");
+    	
+		
+		
+		
+		
 	} else return message.reply("You do not have permission to soft-ban members!");
 	return;
 }
