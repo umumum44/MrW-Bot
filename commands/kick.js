464ecc03fe4835b351e0d5a39e3ba67917f0e0b5
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-02 21:08:46 -0400

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -15,6 +15,7 @@ module.exports.run = async (bot, message, args, prefix) => {
 		}
 		if(!kickeduser) return message.reply("Couldn't find this user!")
 		if(message.member.highestRole.position <= kickeduser.highestRole.position) return message.reply("This user is too high up in this guilds' hierarchy to be kicked by you!");
+		try {
 		kickeduser.send(`You were kicked in ${message.guild.name} for \`${reason}\` by ${message.author.username}`).then(() => {
 			message.guild.member(kickeduser).kick().then(() => {
         			message.react("✅");
@@ -28,6 +29,10 @@ module.exports.run = async (bot, message, args, prefix) => {
 				message.react("❎");
       			});
 		});
+		}
+		catch (e) {
+			message.reply("Couldn't DM this user to tell them they were kicked.")
+		}
 	} else return message.reply("You do not have permission to kick members!");
 	return;
 }
