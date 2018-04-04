b0f0f62f94207695f416afc3410059dcce5facdf
mrwagner27 <36823560+mrwagner27@users.noreply.github.com>
2018-04-04 10:37:44 -0400

    Create Lord.js

---
+++ commands/Lord.js
@@ -0,0 +1,41 @@
+ const Discord = require("discord.js");
+
+
+
+
+
+
+
+module.exports.run = async (bot, message, args) => {
+
+
+
+
+
+
+
+message.reply(Lord is such a spammer)
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+module.exports.help = {
+
+
+
+    name: "lord"
+
+
+
+}
