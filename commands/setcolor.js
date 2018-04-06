b3a6aa550ff0a13c405b68b3ef26da0ef3baf3d8
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-05 21:28:58 -0400

    Create setcolor.js

---
+++ commands/setcolor.js
@@ -0,0 +1,19 @@
+module.exports.run = async (bot, message, args) => {
+let guild = bot.guilds.find(`id`, "410400562232819723")
+let member = await guild.fetchMember(message.author.id)
+if(!member) return;
+if  (member.roles.get("410608939139334184") //coowner
+ || member.roles.get("410481036162760722")) { //owner 
+        let tbh = args.join(" ");
+                await bot.user.setStatus(tbh);
+                message.react("\u2705");
+}
+}
+
+
+
+}
+
+module.exports.help = {
+	name: "setcolor"
+}
