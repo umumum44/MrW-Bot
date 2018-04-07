d8b335b59407890c3bd50d23c8e7a2db3a512a25
mrwagner27 <36823560+mrwagner27@users.noreply.github.com>
2018-04-07 13:04:31 -0400

    Create Windows.js

---
+++ commands/Windows.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+
+
+    message.channel.send("TEST")
+
+    
+
+}
+
+module.exports.help = {
+
+    name: "windows"
+
+}
