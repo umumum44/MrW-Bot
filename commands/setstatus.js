0ddccf7e7b16c17b987537c3eb9d9c413a8b27e7
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-08 13:14:46 -0400

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -3,8 +3,8 @@ module.exports.run = async (bot, message, args) => {
         let guild = bot.guilds.find(`id`, "410400562232819723")
 let member = await guild.fetchMember(message.author.id)
 if(!member) return;
-if  (member.roles.get("410608939139334184") //coowner
- || member.roles.get("410481036162760722")) { //owner 
+if (member.roles.get("410481036162760722")) { //owner 
+
         let tbh = args.join(" ");
                 bot.user.setActivity(`${tbh}`, {type: bot.user.presence.game.type});
                 message.react("\u2705");
