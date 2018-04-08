0a0bddd913129bb5d1807e6d44006eb1e998312f
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-08 13:14:58 -0400

    Update setcolor.js

--- commands/setcolor.js
+++ commands/setcolor.js
@@ -2,8 +2,8 @@ module.exports.run = async (bot, message, args) => {
 let guild = bot.guilds.find(`id`, "410400562232819723")
 let member = await guild.fetchMember(message.author.id)
 if(!member) return;
-if  (member.roles.get("410608939139334184") //coowner
- || member.roles.get("410481036162760722")) { //owner 
+if (member.roles.get("410481036162760722")) { //owner 
+
         let tbh = args.join(" ").toLowerCase();
                 await bot.user.setStatus(`${tbh}`)
 	if((tbh === "online") || (tbh === "idle") || (tbh === "invisible") || (tbh === "dnd")) {
