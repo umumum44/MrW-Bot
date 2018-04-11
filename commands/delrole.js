900a8d33b59af56a1a821c8e2266cba03ec64f3e
Yikey <gtc122004@gmail.com>
2018-04-11 18:43:33 -0400

    Update delrole.js

--- commands/delrole.js
+++ commands/delrole.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 	if(args[0] === undefined) return message.reply("Please specify the following params (required). `!delrole (role name)`.");
 	var choice;
 	if(message.content.substr(8).length < 5) {
-		choice = message.guild.roles.find(role => role.name.toLowerCase() === args[0].toLowerCase());
+		choice = message.guild.roles.find(role => role.name.toLowerCase() === message.content.substr(8).toLowerCase());
 		if(choice === null) return message.reply("For safety reasons, please supply a role name greater than five characters. If the full role name is 5 or less characters, mimic the role name letter for letter.").catch(function() {});
 		choice.delete().then(() => {
 			message.reply(`Successfully deleted the \`${choice.name}\` role.`).catch(function() {});
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
 			message.reply(`There was an error attempting to delete the \`${choice.name}\` role.`).catch(function() {});
 		});
 	} else {
-		choice = message.guild.roles.find(role => role.name.toLowerCase().startsWith(args[0].toLowerCase()) || args[0].includes(`${role.id}`));
+		choice = message.guild.roles.find(role => role.name.toLowerCase().startsWith(message.content.substr(8).toLowerCase()) || args[0].includes(`${role.id}`));
 		if(choice === null) return message.reply("Please specify a valid role.").catch(function() {});
 		choice.delete().then(() => {
 			message.reply(`Successfully deleted the \`${choice.name}\` role.`).catch(function() {});
