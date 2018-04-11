b836548cdea0f84d5caaa4c91e6e4308d286b2da
Yikey <gtc122004@gmail.com>
2018-04-11 18:42:43 -0400

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 	if(args[0] === undefined) return message.reply("Please specify a target. Example: `!giverole @user role`.").catch(function() {});
-	target = message.guild.members.find(member => member.user.tag.toLowerCase().startsWith(args[0].toLowerCase()) || member.user.id === args[0] || message.mentions.users.first() === member.user);
+	target = message.guild.members.find(member => member.user.tag.toLowerCase().startsWith(message.content.substr(5).toLowerCase()) || member.user.id === args[0] || message.mentions.users.first() === member.user);
 	if(target === null) return message.reply("Please specify a valid target.").catch(function() {});
 	if(args[1] === undefined) return message.reply("Please specify a role. Example: `!giverole @user role`.").catch(function() {});
-	var choice = message.guild.roles.find(role => role.name.toLowerCase().startsWith(args[1].toLowerCase()) || args[1].includes(`${role.id}`));
+	var choice = message.guild.roles.find(role => role.name.toLowerCase().startsWith(message.content.substr(5).toLowerCase()) || args[1].includes(`${role.id}`));
 	if(choice === null) return message.reply("Please specify a valid role.").catch(function() {});
 	if(target.roles.has(choice.id)) {
 		target.removeRole(choice).then(() => {
