c49576d9af12d7a674b323d746442d2da62a00fd
Yikey <gtc122004@gmail.com>
2018-04-11 21:03:17 -0400

    Update channelinfo.js

--- commands/channelinfo.js
+++ commands/channelinfo.js
@@ -1,19 +1,19 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
-  let channel = message.channel
+module.exports.run = async (bot, message, args, prefix, content) => {
+	let channel = message.channel
 
-  let channelem = new Discord.RichEmbed()
-  .setTitle(`#${channel.name}`)
-  .addField("Channel ID", channel.id)
-  .addField("Created At", channel.createdAt)
-  .addField("Channel Position", channel.position)
-  .addField("Category", channel.parent)
+	let channelem = new Discord.RichEmbed()
+		.setTitle(`#${channel.name}`)
+		.addField("Channel ID", channel.id)
+		.addField("Created At", channel.createdAt)
+		.addField("Channel Position", channel.position)
+		.addField("Category", channel.parent)
 
 
-  message.channel.send(channelem);
+	message.channel.send(channelem);
 }
 module.exports.help = {
-      name: "channelinfo"
+	name: "channelinfo"
 
-  }
+}
