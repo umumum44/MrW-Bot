cd3de11da30c118bd232a71a607ebf5af1edbf30
Yikey <gtc122004@gmail.com>
2018-04-11 20:55:18 -0400

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message, args, prefix, content) => {
 	var responses = [" no."," highly unlikely."," very doubtful."," you wish."," don't count on it", " probably.", " most definitely", " yes.", " highly likely.", " of course.",
 			 " ask again later.", " outlook not so good, retry.", " ask again later."];
 	var randomchoice = Math.floor(Math.random() * responses.length);
