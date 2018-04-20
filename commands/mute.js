f1fa57be6969be3944d190823a075a7881830e59
Yikey <gtc122004@gmail.com>
2018-04-20 13:53:16 -0400

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,53 +1,60 @@
-const Discord = require("discord.js");
 const ms = require("ms");
+
 module.exports.run = async (bot, message, args, prefix, content) => {
-	if(message.member.hasPermission("KICK_MEMBERS")) {
-		let name = `${args[0]}`;
-		let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-		if(!tomute) {
-			marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName));
-			tomute = marray.first();
-		}
-		if(!tomute) return message.reply("Couldn't find this user!");
-		if(message.member.highestRole.position <= tomute.highestRole.position) return message.reply("This user is too high up in this guilds' hierarchy to be muted by you!");
-		let muterole = message.guild.roles.find(`name`, "Muted");
-		if(!muterole) {
-			try {
-				muterole = await message.guild.createRole({
-					name: "Muted",
-					color: "#6699cc",
-					permissions: []
-				})
-				message.guild.channels.forEach(async (channel, id) => {
-					await channel.overwritePermissions(muterole, {
-						SEND_MESSAGES: false,
-						ADD_REACTIONS: false
+	const parameterOne = args[0];
+	const parameterTwo = args[1];
+	if (message.member.hasPermission("KICK_MEMBERS")) {
+		const target = message.guild.members
+			.find(member => parameterOne.includes(member.user.id) || member.user.tag.toLowerCase().startsWith(parameterOne.toLowerCase()));
+		if (target !== null) {
+			if (message.member.highestRole.position > target.highestRole.position) {
+				if (!target.roles.has(message.guild.roles.find('name', 'Muted').id)) {
+					if (parameterTwo !== undefined) var muteTime = ms(parameterTwo);
+					if (muteTime) {
+						if (muteTime >= 10000) {
+							target.addRole(message.guild.roles.find("name", "Muted")).then(() => {
+								message.channel.send(`***Successfully muted \`${target.user.tag}\` for ${ms(muteTime, { long: true })}.***`).catch(function() {});
+								bot.channels.get("436947091483262996").send(`${target.user.id} ${Date.now() + muteTime}`).then(msg => {
+									setTimeout(() => {
+										target.removeRole(message.guild.roles.find("name", "Muted")).catch(function() {});
+										msg.delete().catch(function() {});
+									}, muteTime);
+								});
+							}).catch(() => {
+								message.channel.send(`Failed to mute \`${target.user.tag}\`.`).catch(function() {});
+							});
+						} else {
+							message.reply("The time to mute the user must be at least 10 seconds.").catch(function() {});
+						}
+					} else {
+						target.addRole(message.guild.roles.find("name", "Muted")).then(() => {
+							message.channel.send(`***Successfully muted \`${target.user.tag}\`.***`).catch(function() {});
+						}).catch(() => {
+							message.channel.send(`Failed to mute \`${target.user.tag}\`.`).catch(function() {});
+						});
+					}
+				} else {
+					message.reply("That user is already muted.").catch(() => {
+						message.author.send(`You attempted to use the \`mute\` command in ${message.channel}, but I can not chat there.`).catch(function() {});
 					});
-				}); 
-			} catch(e) {
-				console.log(e.stack);
-			}}
-		if(tomute.roles.has(muterole.id)) {
-			return message.reply("This user is already muted!");
+				}
+			} else {
+				message.reply("That user is too far up in this guild's hierarchy to be muted by you.").catch(() => {
+					message.author.send(`You attempted to use the \`mute\` command in ${message.channel}, but I can not chat there.`).catch(function() {});
+				});
+			}
 		} else {
-			let mutetime = args[1];
-			if(!mutetime) return message.reply("You must specify a time!");
-			await(tomute.addRole(muterole.id));
-			let reason = message.content.substr(6+prefix.length+args[0].length+args[1].length);
-			if(!reason) reason = "No reason specified";
-			try {
-			await tomute.send(`You were muted in ${message.guild.name} for \`${reason}\` for \`${mutetime}\` by ${message.author.username}`);
-			}
-			catch (e) {
-				await message.reply("Couldn't DM this user to tell them they were muted.")
-			}
-				message.react("\u2705");
-			setTimeout(function(){
-				tomute.removeRole(muterole.id);
-			}, ms(mutetime));
+			message.reply("Please specify a valid user.").catch(() => {
+				message.author.send(`You attempted to use the \`mute\` command in ${message.channel}, but I can not chat there.`).catch(function() {});
+			});
 		}
+	} else {
+		message.reply("You do not have permissions to trigger this command.").catch(() => {
+			message.author.send(`You attempted to use the \`mute\` command in ${message.channel}, but I can not chat there.`).catch(function() {});
+		});
 	}
 }
+
 module.exports.help = {
 	name: "mute"
 }
