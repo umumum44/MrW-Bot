0d1ae3519bc75f0d91dd5dff4115ffaa5838b197
Yikey <gtc122004@gmail.com>
2018-04-20 13:58:49 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,32 +1,59 @@
 const Discord = require("discord.js");
-module.exports.run = async (bot, message, args, prefix, content) => {
-	let name = `${args[0]}`;
-	var reason;
-	if (!reason) {
-		reason = "No reason specified";
-	} else {
-		reason = message.content.substr(4 + prefix.length + args[0].length);
-	}
+
+module.exports.run = async (bot, message, args) => {
+	const rawContent = args.join(" ");
+	const parameterOne = rawContent.split(" ")[0];
+	const parameterTwo = rawContent.split(" ")[1];
 	if (message.member.hasPermission("BAN_MEMBERS")) {
-		let buser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-		if (!buser) {
-			marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName));
-			buser = marray.first();
+		const target = message.guild.members.find(m => parameterOne.includes(`${m.user.id}`));
+		if (target !== null) {
+			if (message.member.highestRole.position > target.highestRole.position) {
+				var reason;
+				if (parameterTwo != undefined) {
+					reason = "`" + rawContent.substr(parameterOne.length + 1) + "`";
+				} else {
+					reason = "`No reason specified.`";
+				}
+				if (target.bannable) {
+					target.send(`You have been banned from the \`${message.guild.name}\` server by \`${message.author.tag}\` for ${reason}`).then(() => {
+						target.ban({
+							days: 7,
+							reason: `Banned by ${message.author.tag} for ${reason}`
+						}).then(() => {
+							message.channel.send(`***Successfully banned \`${target.user.tag}\`.***`).then(msg => msg.delete(5000).catch(function() {}));
+						}).catch(() => {
+							message.channel.send(`Failed to ban \`${target.user.tag}\`.`).then(msg => msg.delete(5000).catch(function() {}));
+						});
+					}).catch(() => {
+						target.ban({
+							days: 7,
+							reason: `Banned by ${message.author.tag} for ${reason}`
+						}).catch(() => {
+							message.channel.send(`Failed to ban \`${target.user.tag}\`.`).then(msg => msg.delete(5000).catch(function() {}));
+						});
+					});
+				} else {
+					message.reply("I do not have permission to ban this user.").catch(() => {
+						message.author.send(`You attempted to use the \`ban\` command in ${message.channel}, but I can not chat there.`)
+							.catch(function() {});
+					});
+				}
+			} else {
+				message.reply("That user is too far up in this guild's hierarchy to be banned by you.").catch(() => {
+					message.author.send(`You attempted to use the \`ban\` command in ${message.channel}, but I can not chat there.`).catch(function() {});
+				});
+			}
+		} else {
+			message.reply("Please mention or supply the id of a valid user.").catch(() => {
+				message.author.send(`You attempted to use the \`ban\` command in ${message.channel}, but I can not chat there.`).catch(function() {});
+			});
 		}
-		if (!buser) return message.reply("Couldn't find that user!")
-		if (message.member.highestRole.position <= buser.highestRole.position) return message.reply("This user is too high up in this guilds' hierarchy to be banned by you!");
-		message.guild.member(buser).ban()
-		try {
-			await buser.send(`You were banned in ${message.guild.name} for \`${reason}\` by ${message.author.username}`);
-			await message.react("✅");
-		} catch (e) {
-			await message.reply("I couldn't DM this user that they were banned.")
-		}
-
-	} else return message.reply("You do not have permission to ban members!");
-	return;
+	} else {
+		message.reply("You do not have permissions to trigger this command.").catch(() => {
+			message.author.send(`You attempted to use the \`ban\` command in ${message.channel}, but I can not chat there.`).catch(function() {});
+		});
+	}
 }
-
 module.exports.help = {
 	name: "ban"
 }
