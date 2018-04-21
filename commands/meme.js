1e2727bf341d5ee41810b112321b42d1dbec044d
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-20 21:13:29 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -7,7 +7,7 @@ let ind = Math.floor(Math.random() * 4);
 let ps = 25
 
 
-getMemeUrls(sq, {pageSize: 100}).then(result => {
+getMemeUrls(sq, {pageSize: 25, pageIndex: 50}).then(result => {
 	console.log(result.length)
   if(!result[0]) return message.reply("Couldn't find memes with this name!")
     var meme = result[Math.floor(Math.random() * result.length)];
