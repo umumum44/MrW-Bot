d49bb9950ca2c0cd588113e930e41eda15340437
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-20 21:34:33 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -5,26 +5,34 @@ let sq = content
 if(!sq) return message.reply("You must provide something to search with!")
 let ind = Math.floor(Math.random() * 250);
 let ps = 25
+let inde = Math.floor(Math.random() * 5);
 
 
-getMemeUrls(sq, {pageSize: 25, pageIndex: 10000}).then(result => {
-  if(!result[0]) {
-	  let inde = Math.floor(Math.random() * 10);
-	getMemeUrls(sq, {pageSize: 25, pageIndex: inde}).then(resulto => {
-		 if(!resulto[0]) {
-	  return message.reply("Couldn't find memes with this name!")
- }
-		var memeo = result[Math.floor(Math.random() * result.length)];
-	message.reply(memeo)
-	}).catch(err => {
-console.log(err)
- });
-	  
+getMemeUrls(sq, {pageSize: 25, pageIndex: ind}).then(result => {
+  if(result[0]) {
     var meme = result[Math.floor(Math.random() * result.length)];
-	message.reply(meme)
+	return message.reply(meme)
+  }
   }).catch(err => {
 console.log(err)
  });
+	
+	
+	
+	getMemeUrls(sq, {pageSize: 25, pageIndex: inde}).then(resulto => {
+  if(!resulto[0]) {
+	 	  return message.reply("Couldn't find memes with this name!")
+ }
+		var memeo = resulto[Math.floor(Math.random() * resulto.length)];
+	message.reply(memeo)
+	}).catch(err => {
+console.log(err)
+  }).catch(err => {
+console.log(err)
+ });
+		
+		
+		
 
 }
 module.exports.help = {
