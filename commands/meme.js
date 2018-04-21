d49345e18d5d4565b147d1d88eec2e8f6b608dea
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-20 21:44:36 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix, content) => {
 let sq = content
 if(!sq) return message.reply("You must provide something to search with!")
-let ind = Math.floor(Math.random() * 250);
+let ind = Math.floor(Math.random() * 400);
 let ps = 25
-let inde = Math.floor(Math.random() * 5);
+let inde = Math.floor(Math.random() * 20);
 
  let pp = await new Promise((resolve, reject) => {
 getMemeUrls(sq, {pageSize: 25, pageIndex: ind}).then(result => {
