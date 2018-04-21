3cf75ed82f3224a19c9c1935e0f3a4fbf606295b
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-20 20:28:24 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -4,10 +4,11 @@ module.exports.run = async (bot, message, args, prefix, content) => {
 let sq = content
 if(!sq) return message.reply("You must provide something to search with!")
 let ind = Math.floor(Math.random() * 4);
-let ps = 25
+let ps = 100
 
 
 getMemeUrls(sq, [{pageSize: ps}, {pageIndex: ind}]).then(result => {
+	console.log(result.size)
   if(!result[0]) return message.reply("Couldn't find memes with this name!")
     var meme = result[Math.floor(Math.random() * result.length)];
    /* let memee = new Discord.RichEmbed()
