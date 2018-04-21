d7bf1021c06e1fb31958cf247d7b1501e2b9bfa2
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-20 21:04:06 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix, content) => {
+let msg = message.cleanContent
+msg.substr(message.content.length-content.length)
 message.channel.send(message.cleanContent)
 message.delete()
 }
