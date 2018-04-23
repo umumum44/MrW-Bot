0b13d45cea4aaca9ad585a4b9d7b8acaa82b52b9
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-22 21:01:17 -0400

    Update countstatus.js

--- commands/countstatus.js
+++ commands/countstatus.js
@@ -1,26 +1,24 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix, content) => {
-	let guild = bot.guilds.find(`id`, "410400562232819723")
-	let member = await guild.fetchMember(message.author.id)
-	if (!member) return;
-	if (member.roles.get("410481036162760722")) { //owner 
-		if (bot.counter === false) {
-			bot.user.setActivity(`${bot.guilds.size} servers`, {
-				type: "WATCHING"
-			});
-			message.react("\u2705")
-			bot.counter = true
-		} else if (bot.counter === true) {
-			bot.user.setActivity("Games", {
-				type: "PLAYING"
-			});
-			message.react("\u2705")
-
-			bot.counter = false
-		}
-	}
+        let guild = bot.guilds.find(`id`, "410400562232819723")
+        let member = await guild.fetchMember(message.author.id)
+        if (!member) return;
+        if (member.roles.get("410481036162760722")) { //owner 
+                if (bot.counter === false) {
+                        bot.user.setActivity(`${bot.guilds.size} servers`, {
+                                type: "WATCHING"
+                        });
+                        message.react("\u2705")
+                        bot.counter = true
+                } else if (bot.counter === true) {
+                        bot.user.setActivity("Games", {
+                                type: "PLAYING"
+                        });
+                        message.react("\u2705")
+                        bot.counter = false
+                }
+        }
 }
-
 module.exports.help = {
-	name: "countstatus"
+        name: "countstatus"
 }
