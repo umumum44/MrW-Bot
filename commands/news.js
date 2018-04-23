f112e2e17d6a4f2649dd454092ac0ad9072bf004
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-22 21:03:10 -0400

    Update news.js

--- commands/news.js
+++ commands/news.js
@@ -1,14 +1,13 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix, content) => {
-	let channel = bot.channels.find(`id`, "430477691154595852")
-	let editor = await channel.fetchMessage("430478034160713728")
-	let thing = new Discord.RichEmbed()
-		.setTitle("News")
-		.setDescription(editor.content)
-		.setFooter(`Requested by ${message.author.tag}`)
-	await message.channel.send(thing)
+        let channel = bot.channels.find(`id`, "430477691154595852")
+        let editor = await channel.fetchMessage("430478034160713728")
+        let thing = new Discord.RichEmbed()
+                .setTitle("News")
+                .setDescription(editor.content)
+                .setFooter(`Requested by ${message.author.tag}`)
+        await message.channel.send(thing)
 }
-
 module.exports.help = {
-	name: "news"
+        name: "news"
 }
