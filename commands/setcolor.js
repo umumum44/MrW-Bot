4f7e733b7f5a9c20cd3b283e37ecf85806405a5b
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-22 21:06:30 -0400

    Update setcolor.js

--- commands/setcolor.js
+++ commands/setcolor.js
@@ -1,23 +1,18 @@
 module.exports.run = async (bot, message, args) => {
-let guild = bot.guilds.find(`id`, "410400562232819723")
-let member = await guild.fetchMember(message.author.id)
-if(!member) return;
-if (member.roles.get("410481036162760722")) { //owner 
-
-        let tbh = args.join(" ").toLowerCase();
+        let guild = bot.guilds.find(`id`, "410400562232819723")
+        let member = await guild.fetchMember(message.author.id)
+        if (!member) return;
+        if (member.roles.get("410481036162760722")) { //owner 
+                let tbh = args.join(" ")
+                        .toLowerCase();
                 await bot.user.setStatus(`${tbh}`)
-	if((tbh === "online") || (tbh === "idle") || (tbh === "invisible") || (tbh === "dnd")) {
-                await message.react("\u2705");
-	} else {
-		message.reply("Not a valid option!\nOptions: \n**online\nidle\ninvisible\ndnd**")
-	}
+                if ((tbh === "online") || (tbh === "idle") || (tbh === "invisible") || (tbh === "dnd")) {
+                        await message.react("\u2705");
+                } else {
+                        message.reply("Not a valid option!\nOptions: \n**online\nidle\ninvisible\ndnd**")
+                }
+        }
 }
-
-
-
-
-}
-
 module.exports.help = {
-	name: "setcolor"
+        name: "setcolor"
 }
