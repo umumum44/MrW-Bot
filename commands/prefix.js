dd004c1506946ccf56a964c652430eacef23673c
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-22 21:03:51 -0400

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,42 +1,43 @@
 const Discord = require("discord.js");
-
 module.exports.run = async (bot, message, args) => {
-
-if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply("You don't have permission to use this command!")
-if(!args[0]) return message.reply("Please provide the new prefix!")
-let prefix = args[0]
-if(prefix.length > 5) return message.reply("The prefix cannot be more than 5 characters!")
-let dbguild = bot.guilds.find(`id`, "417149156193337344");
-let channel = dbguild.channels.find(`name`, "wbotprefixes-database")
-let messages = await channel.fetchMessages({ limit: 100 })
-let channels = dbguild.channels.filter(m => RegExp("wbotprefixes-database", "gi").test(m.name));
-channels.forEach(chl => {
-  chl.fetchMessages({ limit: 100 }).then(msgs => {
-      msgs.forEach(msg => {
-        if(msg.content.startsWith(`${message.guild.id}`)) {
-          msg.delete()
+        if (!message.member.hasPermission("MANAGE_GUILD")) return message.reply("You don't have permission to use this command!")
+        if (!args[0]) return message.reply("Please provide the new prefix!")
+        let prefix = args[0]
+        if (prefix.length > 5) return message.reply("The prefix cannot be more than 5 characters!")
+        let dbguild = bot.guilds.find(`id`, "417149156193337344");
+        let channel = dbguild.channels.find(`name`, "wbotprefixes-database")
+        let messages = await channel.fetchMessages({
+                limit: 100
+        })
+        let channels = dbguild.channels.filter(m => RegExp("wbotprefixes-database", "gi")
+                .test(m.name));
+        channels.forEach(chl => {
+                chl.fetchMessages({
+                                limit: 100
+                        })
+                        .then(msgs => {
+                                msgs.forEach(msg => {
+                                        if (msg.content.startsWith(`${message.guild.id}`)) {
+                                                msg.delete()
+                                        }
+                                })
+                        })
+        })
+        if (messages.size === 100) {
+                await channel.setName("o-wbotprefixes-database")
+                await dbguild.createChannel('wbotprefixes-database')
+                let newc = dbguild.channels.find(`name`, "wbotprefixes-database")
+                await newc.overwritePermissions(channel.guild.id, {
+                        READ_MESSAGES: false
+                })
+                await newc.setParent("422122104499208214");
+                await newc.send(`${message.guild.id} ${prefix}`);
+                message.react("\u2705")
+        } else {
+                channel.send(`${message.guild.id} ${prefix}`)
+                message.react("\u2705")
         }
-      })
-    })
-  })
-if(messages.size === 100) {
-await channel.setName("o-wbotprefixes-database")
-await dbguild.createChannel('wbotprefixes-database')
-  let newc = dbguild.channels.find(`name`, "wbotprefixes-database")
-  await newc.overwritePermissions(channel.guild.id, {
-  READ_MESSAGES: false
-})
-          await newc.setParent("422122104499208214");
-          await newc.send(`${message.guild.id} ${prefix}`);
-  		      message.react("\u2705")
-
-
-} else {
-  channel.send(`${message.guild.id} ${prefix}`)
-  		      message.react("\u2705")
 }
- }
-
 module.exports.help = {
-    name: "prefix"
+        name: "prefix"
 }
