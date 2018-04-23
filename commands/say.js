77847d03674d450f54541e6092354f3218ad3044
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-22 21:05:35 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix, content) => {
-let msg = message.cleanContent
-let msssg = msg.substr(message.content.length-content.length)
-message.channel.send(msssg)
-message.delete()
+        let msg = message.cleanContent
+        let msssg = msg.substr(message.content.length - content.length)
+        message.channel.send(msssg)
+        message.delete()
 }
 module.exports.help = {
-	name: "say"
+        name: "say"
 }
