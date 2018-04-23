e6a55d5bc1edd052f258eb6f296a031a3a7737fb
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-22 21:07:27 -0400

    Update unblacklist.js

--- commands/unblacklist.js
+++ commands/unblacklist.js
@@ -1,37 +1,37 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-let guild = bot.guilds.find(`id`, "410400562232819723")
-let member = await guild.fetchMember(message.author.id)
-if(!member) return;
-if (member.roles.get("410546480307503124") //mod
- || member.roles.get("410611296401358848") //admin
- || member.roles.get("410608939139334184") //coowner
- || member.roles.get("410481036162760722")) { //owner 
-      let channel = bot.channels.find(`id`, "424006591411519499")
-      let pingeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-	let userid = args[0]
-	let messages = await channel.fetchMessages({ limit: 100 })
-
-      if(!pingeduser) {
-      	      let barray = messages.filter(m => RegExp(userid, "gi").test(m.content));
-	      let auser = barray.first();
-	      if(auser) {
-		     auser.delete()
-		     message.react("\u2705")
-	      } else return message.reply("This user is not blacklisted!")
-      } else {
-      	      let darray = messages.filter(m => RegExp(pingeduser.id, "gi").test(m.content));
-	      let buser = darray.first();
-	if(buser) { 
-		buser.delete()
-		message.react("\u2705")
-	} else return message.reply("This user is not blacklisted!")
+        let guild = bot.guilds.find(`id`, "410400562232819723")
+        let member = await guild.fetchMember(message.author.id)
+        if (!member) return;
+        if (member.roles.get("410546480307503124") //mod
+                || member.roles.get("410611296401358848") //admin
+                || member.roles.get("410608939139334184") //coowner
+                || member.roles.get("410481036162760722")) { //owner 
+                let channel = bot.channels.find(`id`, "424006591411519499")
+                let pingeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+                let userid = args[0]
+                let messages = await channel.fetchMessages({
+                        limit: 100
+                })
+                if (!pingeduser) {
+                        let barray = messages.filter(m => RegExp(userid, "gi")
+                                .test(m.content));
+                        let auser = barray.first();
+                        if (auser) {
+                                auser.delete()
+                                message.react("\u2705")
+                        } else return message.reply("This user is not blacklisted!")
+                } else {
+                        let darray = messages.filter(m => RegExp(pingeduser.id, "gi")
+                                .test(m.content));
+                        let buser = darray.first();
+                        if (buser) {
+                                buser.delete()
+                                message.react("\u2705")
+                        } else return message.reply("This user is not blacklisted!")
+                }
+        }
 }
-	
-}
-
-}
-
 module.exports.help = {
-	name: "unblacklist"
+        name: "unblacklist"
 }
