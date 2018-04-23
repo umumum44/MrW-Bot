5061685ad5e090076e0e3249b0627f57eea4fd43
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-22 21:00:12 -0400

    Update blacklist.js

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -1,46 +1,41 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix, content) => {
-	let guild = bot.guilds.find(`id`, "410400562232819723")
-	let member = await guild.fetchMember(message.author.id)
-	if (!member) return;
-	if (member.roles.get("410546480307503124") //mod
-		||
-		member.roles.get("410611296401358848") //admin
-		||
-		member.roles.get("410608939139334184") //coowner
-		||
-		member.roles.get("410481036162760722")) { //owner 
-		let channel = bot.channels.find(`id`, "424006591411519499")
-		let pingeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-		let userid = args[0]
-		let messages = await channel.fetchMessages({
-			limit: 100
-		})
-
-		if (!pingeduser) {
-			let barray = messages.filter(m => RegExp(userid, "gi").test(m.content));
-			let auser = barray.first();
-			if (!auser) {
-				let userob = await bot.fetchUser(userid)
-				if (!userob) return message.reply("Couldn't find this user!")
-				channel.send(`${userid}, ${userob.username}#${userob.discriminator}`)
-				message.react("\u2705")
-			} else return message.reply("This user is already blacklisted!")
-		} else {
-			let darray = messages.filter(m => RegExp(pingeduser.id, "gi").test(m.content));
-			let buser = darray.first();
-			if (!buser) {
-				let userob = await bot.fetchUser(pingeduser.id)
-				if (!userob) message.reply("Couldn't find this user!")
-				channel.send(`${pingeduser.id}, ${userob.username}#${userob.discriminator}`)
-				message.react("\u2705")
-			} else return message.reply("This user is already blacklisted!")
-		}
-
-	}
-
+        let guild = bot.guilds.find(`id`, "410400562232819723")
+        let member = await guild.fetchMember(message.author.id)
+        if (!member) return;
+        if (member.roles.get("410546480307503124") //mod
+                || member.roles.get("410611296401358848") //admin
+                || member.roles.get("410608939139334184") //coowner
+                || member.roles.get("410481036162760722")) { //owner 
+                let channel = bot.channels.find(`id`, "424006591411519499")
+                let pingeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+                let userid = args[0]
+                let messages = await channel.fetchMessages({
+                        limit: 100
+                })
+                if (!pingeduser) {
+                        let barray = messages.filter(m => RegExp(userid, "gi")
+                                .test(m.content));
+                        let auser = barray.first();
+                        if (!auser) {
+                                let userob = await bot.fetchUser(userid)
+                                if (!userob) return message.reply("Couldn't find this user!")
+                                channel.send(`${userid}, ${userob.username}#${userob.discriminator}`)
+                                message.react("\u2705")
+                        } else return message.reply("This user is already blacklisted!")
+                } else {
+                        let darray = messages.filter(m => RegExp(pingeduser.id, "gi")
+                                .test(m.content));
+                        let buser = darray.first();
+                        if (!buser) {
+                                let userob = await bot.fetchUser(pingeduser.id)
+                                if (!userob) message.reply("Couldn't find this user!")
+                                channel.send(`${pingeduser.id}, ${userob.username}#${userob.discriminator}`)
+                                message.react("\u2705")
+                        } else return message.reply("This user is already blacklisted!")
+                }
+        }
 }
-
 module.exports.help = {
-	name: "blacklist"
+        name: "blacklist"
 }
