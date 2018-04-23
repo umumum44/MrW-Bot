defe57b7ee81151f5cddbed83ec368fe278d3ec3
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-22 21:06:44 -0400

    Update setnews.js

--- commands/setnews.js
+++ commands/setnews.js
@@ -1,20 +1,15 @@
 module.exports.run = async (bot, message, args, prefix) => {
-	let guild = bot.guilds.find(`id`, "410400562232819723")
-let member = await guild.fetchMember(message.author.id)
-if(!member) return;
-if (member.roles.get("410481036162760722")) { //owner 
-  let update = message.content.substr(prefix.length + 8);
-
-let channel = bot.channels.find(`id`, "430477691154595852")
-let editor = await channel.fetchMessage("430478034160713728")
-await editor.edit(update)
-await message.react("\u2705")
-
-
-}
-
-
+        let guild = bot.guilds.find(`id`, "410400562232819723")
+        let member = await guild.fetchMember(message.author.id)
+        if (!member) return;
+        if (member.roles.get("410481036162760722")) { //owner 
+                let update = message.content.substr(prefix.length + 8);
+                let channel = bot.channels.find(`id`, "430477691154595852")
+                let editor = await channel.fetchMessage("430478034160713728")
+                await editor.edit(update)
+                await message.react("\u2705")
+        }
 }
 module.exports.help = {
-	name: "setnews"
+        name: "setnews"
 }
