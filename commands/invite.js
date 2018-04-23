fff2cdfe54ce8ac1a34628ddd3c7853df85d0b7a
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-22 21:01:58 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
-
 module.exports.run = async (bot, message, args, prefix, content) => {
-      message.reply("**Invite me here!:** https://discordapp.com/oauth2/authorize?client_id=419881218784493588&permissions=347201&scope=bot")
+        message.reply("**Invite me here!:** https://discordapp.com/oauth2/authorize?client_id=419881218784493588&permissions=347201&scope=bot")
 }
 module.exports.help = {
-      name: "invite"
+        name: "invite"
 }
