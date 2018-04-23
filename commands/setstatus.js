b8f558ded0f5d46fc7581e6d7a71c05328594e12
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-22 21:06:58 -0400

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -1,18 +1,16 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
         let guild = bot.guilds.find(`id`, "410400562232819723")
-let member = await guild.fetchMember(message.author.id)
-if(!member) return;
-if (member.roles.get("410481036162760722")) { //owner 
-
-        let tbh = args.join(" ");
-                bot.user.setActivity(`${tbh}`, {type: bot.user.presence.game.type});
+        let member = await guild.fetchMember(message.author.id)
+        if (!member) return;
+        if (member.roles.get("410481036162760722")) { //owner 
+                let tbh = args.join(" ");
+                bot.user.setActivity(`${tbh}`, {
+                        type: bot.user.presence.game.type
+                });
                 message.react("\u2705");
-      
-
+        }
 }
-}
-
 module.exports.help = {
         name: "setstatus"
 }
