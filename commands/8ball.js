2557a73b896d10f2c7329c20aebddeaa41399c40
Windows <36823560+mrwagner27@users.noreply.github.com>
2018-04-24 20:20:10 -0400

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix, content) => {
         var responses = [" no.", " highly unlikely.", " very doubtful.", " you wish.", " don't count on it", " probably.", " most definitely", " yes.", " highly likely.", " of course."
-			 , " ask again later.", " outlook not so good, retry.", " ask again later."];
+             , " ask again later.", " outlook not so good, retry.", " ask again later.", " better not tell you now."];
         var randomchoice = Math.floor(Math.random() * responses.length);
-        message.reply("The 8ball says... " + "**" + responses[randomchoice] + "**");
+        message.reply("The 8ball says... " + "" + responses[randomchoice] + "");
 }
 module.exports.help = {
         name: "8ball"
