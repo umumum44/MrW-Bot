b2a7d089a9e32baca08ab563f0b367854aac7cc4
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-25 21:34:19 -0400

    Update announcer.js

--- commandsinprogress/announcer.js
+++ commandsinprogress/announcer.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+/*const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix, content) => {
 let togglechannel
         if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply("You don't have permission to use this command!")
