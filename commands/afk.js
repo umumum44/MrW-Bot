100c72a06ba585c98b3948b0cc1f33c527ee0ca7
Windows <36823560+mrwagner27@users.noreply.github.com>
2018-04-27 14:55:41 -0400

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args, prefix, content) => {
         if (!afkmsg) afkmsg = "No reason provided.";
         let channel = bot.channels.find(`id`, "422201325623836682");
         channel.send(`${message.author.id} ${afkmsg}`);
-        message.reply(`You are now AFK!!!: \`${afkmsg}\`\nTo become un-AFK, just talk again!`);
+        message.reply(`You are now AFK!!!: \`${afkmsg}\`\nTo become un-AFK, just talk again!!`);
         message.delete()
 }
 module.exports.help = {
