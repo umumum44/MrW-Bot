a517b05c719f81ecc480305ee19cc5475a4f213d
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-27 17:48:53 -0400

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,5 +1,20 @@
 const Discord = require("discord.js");
+async function checkIfDisabled(bot, message, args, cmdname, channels) {
+                const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({
+                        limit: 100
+                })))
+                const flatMessages = nestedMessages.reduce((a, b) => a.concat(b))
+                const msg = flatMessages.find(msg => msg.content.startsWith(`${message.guild.id} ${cmdname}`))
+		if(msg) {
+			return(true)
+		} else {
+			return(false)
+		}
+}
 module.exports.run = async (bot, message, args) => {
+         let channels = dbguild.channels.filter(m => RegExp("wbotdisable-database", "gi").test(m.name));
+	let cmddisablecheck = await checkIfDisabled(bot, message, args, "purge", channels)
+	if(cmddisablecheck) return message.reply("This command has been disabled by a server manager!")
         if (message.member.hasPermission("MANAGE_MESSAGES")) {
                 let num = parseInt(args[0]);
                 if (!num) return message.reply("You must provide the number of messages to delete!");
