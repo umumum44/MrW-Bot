0ffbf53a0b71afb4d12adbe5e6e011d8a611dade
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-27 17:41:18 -0400

    Update countstatus.js

--- commands/countstatus.js
+++ commands/countstatus.js
@@ -1,5 +1,20 @@
 const Discord = require("discord.js");
+async function checkIfDisabled(bot, message, args, cmdname, channels) {
+                const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({
+                        limit: 100
+                })))
+                const flatMessages = nestedMessages.reduce((a, b) => a.concat(b))
+                const msg = flatMessages.find(msg => msg.content.startsWith(`${message.guild.id} ${cmdname}`))
+		if(msg) {
+			return(true)
+		} else {
+			return(false)
+		}
+}
 module.exports.run = async (bot, message, args, prefix, content) => {
+        let channels = dbguild.channels.filter(m => RegExp("wbotdisable-database", "gi").test(m.name));
+	let cmddisablecheck = await checkIfDisabled(bot, message, args, "countstatus", channels)
+	if(cmddisablecheck) return message.reply("This command has been disabled by a server manager!")
         let guild = bot.guilds.find(`id`, "410400562232819723")
         let member = await guild.fetchMember(message.author.id)
         if (!member) return;
