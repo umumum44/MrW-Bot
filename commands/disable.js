72d08ea133f65ca7b111cbff3b4c724f5bcd9abd
Windows <36823560+mrwagner27@users.noreply.github.com>
2018-04-26 22:05:59 -0400

    Create disable.js

---
+++ commands/disable.js
@@ -0,0 +1,40 @@
+Why does this code not work const Discord = require("discord.js");
+
+
+
+
+
+
+
+module.exports.run = async (bot, message, args) => {
+
+
+
+
+
+
+
+message.reply("Coming soon™);
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+module.exports.help = {
+
+
+
+    name: "disable"
+
+
+
+}
