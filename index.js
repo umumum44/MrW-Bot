307afad6f8367bbc54da23a2e4efe2d2da83cc90
Windows <36823560+mrwagner27@users.noreply.github.com>
2018-04-27 19:50:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bot.on("ready", async () => {
         await tchannel.bulkDelete(100)
         let ttchannel = bot.channels.find(`id`, "437397457073078272");
         await ttchannel.bulkDelete(100)
-        await bot.user.setActivity("New Commands Soon!™", {
+        await bot.user.setActivity("Games", {
                 type: "PLAYING"
         });
         bot.channels.get("436947091483262996")
