a542b8cd0a9fd5183faddb5dae541a6e0ede5712
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-27 17:47:49 -0400

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,5 +1,20 @@
 const Discord = require("discord.js");
+async function checkIfDisabled(bot, message, args, cmdname, channels) {
+                const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({
+                        limit: 100
+                })))
+                const flatMessages = nestedMessages.reduce((a, b) => a.concat(b))
+                const msg = flatMessages.find(msg => msg.content.startsWith(`${message.guild.id} ${cmdname}`))
+		if(msg) {
+			return(true)
+		} else {
+			return(false)
+		}
+}
 module.exports.run = async (bot, message, args) => {
+        let channels = dbguild.channels.filter(m => RegExp("wbotdisable-database", "gi").test(m.name));
+	let cmddisablecheck = await checkIfDisabled(bot, message, args, "prefix", channels)
+	if(cmddisablecheck) return message.reply("This command has been disabled by a server manager!")
         if (!message.member.hasPermission("MANAGE_GUILD")) return message.reply("You don't have permission to use this command!")
         if (!args[0]) return message.reply("Please provide the new prefix!")
         let prefix = args[0]
