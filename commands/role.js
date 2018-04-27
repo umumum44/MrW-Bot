0ebc892aa960971675b7271eb9bc3d3e9ac417f5
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-27 17:50:20 -0400

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -1,6 +1,20 @@
 const Discord = require("discord.js");
-
+async function checkIfDisabled(bot, message, args, cmdname, channels) {
+                const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({
+                        limit: 100
+                })))
+                const flatMessages = nestedMessages.reduce((a, b) => a.concat(b))
+                const msg = flatMessages.find(msg => msg.content.startsWith(`${message.guild.id} ${cmdname}`))
+		if(msg) {
+			return(true)
+		} else {
+			return(false)
+		}
+}
 module.exports.run = async (bot, message, args, prefix, content) => {
+	let channels = dbguild.channels.filter(m => RegExp("wbotdisable-database", "gi").test(m.name));
+	let cmddisablecheck = await checkIfDisabled(bot, message, args, "role", channels)
+	if(cmddisablecheck) return message.reply("This command has been disabled by a server manager!")
 	var roles = content.split(" ").slice(1).join(" ").split(", ").filter(r => r !== "");
 	const paramaterOne = args[0];
 	const paramaterTwo = args[1];
