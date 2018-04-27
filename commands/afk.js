10fb759e2fafb0bd4ba802fe685dbee44b33cd18
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-27 17:33:09 -0400

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -1,4 +1,19 @@
+async function checkIfDisabled(bot, message, args, cmdname, channels) {
+                const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({
+                        limit: 100
+                })))
+                const flatMessages = nestedMessages.reduce((a, b) => a.concat(b))
+                const msg = flatMessages.find(msg => msg.content.startsWith(`${message.guild.id} ${cmdname}`))
+		if(msg) {
+			return(true)
+		} else {
+			return(false)
+		}
+}
 module.exports.run = async (bot, message, args, prefix, content) => {
+        let channels = dbguild.channels.filter(m => RegExp("wbotdisable-database", "gi").test(m.name));
+	let cmddisablecheck = await checkIfDisabled(bot, message, args, "8ball", channels)
+	if(cmddisablecheck) return message.reply("This command has been disabled by a server manager!")
         let afkmsg = args.join(" ");
         if (!afkmsg) afkmsg = "No reason provided.";
         let channel = bot.channels.find(`id`, "422201325623836682");
