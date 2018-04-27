33427f75086437a3dc993830a403b953dcdd27c4
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-27 17:34:59 -0400

    Update autoroles.js

--- commands/autoroles.js
+++ commands/autoroles.js
@@ -1,6 +1,20 @@
 const Discord = require("discord.js");
-
+async function checkIfDisabled(bot, message, args, cmdname, channels) {
+                const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({
+                        limit: 100
+                })))
+                const flatMessages = nestedMessages.reduce((a, b) => a.concat(b))
+                const msg = flatMessages.find(msg => msg.content.startsWith(`${message.guild.id} ${cmdname}`))
+		if(msg) {
+			return(true)
+		} else {
+			return(false)
+		}
+}
 module.exports.run = async (bot, message, args, prefix, content) => {
+	let channels = dbguild.channels.filter(m => RegExp("wbotdisable-database", "gi").test(m.name));
+	let cmddisablecheck = await checkIfDisabled(bot, message, args, "8ball", channels)
+	if(cmddisablecheck) return message.reply("This command has been disabled by a server manager!")
 	if (!message.member.hasPermission("MANAGE_GUILD")) return message.reply("You do not have permissions to use this command.");
 	var dbguild = bot.guilds.get("417149156193337344");
 	var dbchannels = dbguild.channels.filter(channel => channel.name.includes("autoroles-database"));
