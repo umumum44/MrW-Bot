25f80252a3dd23f186c4475d033ce73bb1992df4
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-26 22:08:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ bot.on("guildMemberAdd", async member => {
 	if(togglesetting === "false") return;
 	if(channelsetting === "none") return;
 	if(hellomsg !== "none") {
-	var himessage = hellomsg.replace(/{user}/i, `${member.user.toString()}`);
+	var himessage = hellomsg.replace(/{user}/gi, `${member.user.toString()}`);
 	    } else {
 		    var himessage = `Welcome to ${member.guild.name}, ${member.user.toString()}! Have a good time here!`
 	    }
@@ -154,7 +154,7 @@ bot.on("guildMemberRemove", async member => {
 	if(togglesetting === "false") return;
 	if(channelsetting === "none") return;
 	if(byemsg !== "none") {
-	var byemessage = byemsg.replace(/{user}/i, `${member.user.toString()}`);
+	var byemessage = byemsg.replace(/{user}/gi, `${member.user.toString()}`);
 	    } else {
 		    var byemessage = `Sad to see you leave ${member.user.toString()}.`
 	    }
