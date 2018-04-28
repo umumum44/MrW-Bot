3dd260a462a8fea97a5b1c4be342365edce8391d
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-27 20:28:15 -0400

    Update removewarn.js

--- commands/removewarn.js
+++ commands/removewarn.js
@@ -1,20 +1,7 @@
 const Discord = require("discord.js");
-async function checkIfDisabled(bot, message, args, cmdname, channels) {
-                const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({
-                        limit: 100
-                })))
-                const flatMessages = nestedMessages.reduce((a, b) => a.concat(b))
-                const msg = flatMessages.find(msg => msg.content.startsWith(`${message.guild.id} ${cmdname}`))
-		if(msg) {
-			return(true)
-		} else {
-			return(false)
-		}
-}
+
 module.exports.run = async (bot, message, args) => {
-   let channels = dbguild.channels.filter(m => RegExp("wbotdisable-database", "gi").test(m.name));
-	let cmddisablecheck = await checkIfDisabled(bot, message, args, "removewarn", channels)
-	if(cmddisablecheck) return message.reply("This command has been disabled by a server manager!")
+
   let target = message.guild.member(message.mentions.users.first());
   if (target) {
     if(message.member.hasPermission("KICK_MEMBERS") || message.member.hasPermission("ADMINISTRATOR") || message.member.hasPermission("BAN_MEMBERS")) {
