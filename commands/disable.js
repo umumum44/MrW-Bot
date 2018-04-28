33defcf74af740e37c18274c35705660c8dce875
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-27 20:39:03 -0400

    Update disable.js

--- commands/disable.js
+++ commands/disable.js
@@ -12,6 +12,7 @@ async function checkIfDisabled(bot, message, args, cmdname, channels) {
 		}
 }
 module.exports.run = async (bot, message, args, prefix, content) => {
+	var nodisable = ["help", "disable"]
 if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply("You do not have permission to use this command!")
 	if(!args[0]) return message.reply("You must provide the name of the command to enable/disable! Please try again.")
 	//if(args[0] === "disable" || "help") return message.reply("This command cannot be disabled!")
@@ -20,8 +21,9 @@ if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply("You do n
         let messages = await channel.fetchMessages({
                 limit: 100
         })
+	if(nodisable.includes(args[0].toLowerCase())) return message.reply("This command cannot be disabled!")
 	let channels = dbguild.channels.filter(m => RegExp("wbotdisable-database", "gi").test(m.name));
-	let cmddisablecheck = await checkIfDisabled(bot, message, args, args[0], channels)
+	let cmddisablecheck = await checkIfDisabled(bot, message, args, args[0].toLowerCase(), channels)
 	if(cmddisablecheck) {
         channels.forEach(chl => {
                 chl.fetchMessages({
@@ -29,7 +31,7 @@ if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply("You do n
                         })
                         .then(msgs => {
                                 msgs.forEach(msg => {
-                                        if (msg.content.startsWith(`${message.guild.id} ${args[0]}`)) {
+                                        if (msg.content.startsWith(`${message.guild.id} ${args[0].toLowerCase()}`)) {
                                                 msg.delete()
                                         }
                                 })
@@ -38,7 +40,7 @@ if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply("You do n
 		message.reply("Re-enabled the command!")
 	}
 	if(!cmddisablecheck) {
-		await channel.send(`${message.guild.id} ${args[0]}`)
+		await channel.send(`${message.guild.id} ${args[0].toLowerCase()}`)
 		await message.reply("Disabled the command!")
 	}
 	if(message.size > 97) {
