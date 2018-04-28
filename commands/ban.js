a529d5b5945293c037b275e40a7913e536eced2b
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-27 20:21:52 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,20 +1,7 @@
 const Discord = require("discord.js");
-async function checkIfDisabled(bot, message, args, cmdname, channels) {
-                const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({
-                        limit: 100
-                })))
-                const flatMessages = nestedMessages.reduce((a, b) => a.concat(b))
-                const msg = flatMessages.find(msg => msg.content.startsWith(`${message.guild.id} ${cmdname}`))
-		if(msg) {
-			return(true)
-		} else {
-			return(false)
-		}
-}
+
 module.exports.run = async (bot, message, args) => {
-	 let channels = dbguild.channels.filter(m => RegExp("wbotdisable-database", "gi").test(m.name));
-	let cmddisablecheck = await checkIfDisabled(bot, message, args, "ban", channels)
-	if(cmddisablecheck) return message.reply("This command has been disabled by a server manager!")
+	 
 	const rawContent = args.join(" ");
 	const parameterOne = rawContent.split(" ")[0];
 	const parameterTwo = rawContent.split(" ")[1];
