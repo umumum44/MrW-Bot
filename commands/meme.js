e45685c4161efbe48851488996f1d3db0bc08834
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-27 20:25:22 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,21 +1,8 @@
 const getMemeUrls = require('get-meme-urls');
 const Discord = require("discord.js");
-async function checkIfDisabled(bot, message, args, cmdname, channels) {
-                const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({
-                        limit: 100
-                })))
-                const flatMessages = nestedMessages.reduce((a, b) => a.concat(b))
-                const msg = flatMessages.find(msg => msg.content.startsWith(`${message.guild.id} ${cmdname}`))
-		if(msg) {
-			return(true)
-		} else {
-			return(false)
-		}
-}
+
 module.exports.run = async (bot, message, args, prefix, content) => {
-        let channels = dbguild.channels.filter(m => RegExp("wbotdisable-database", "gi").test(m.name));
-	let cmddisablecheck = await checkIfDisabled(bot, message, args, "meme", channels)
-	if(cmddisablecheck) return message.reply("This command has been disabled by a server manager!")
+     
         let sq = content
         if (!sq) return message.reply("You must provide something to search with!")
         let ind = Math.floor(Math.random() * 400);
