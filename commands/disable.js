9584702f34d938ffc859679b183d77f5f7eb5770
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-28 18:12:12 -0400

    Update disable.js

--- commands/disable.js
+++ commands/disable.js
@@ -19,7 +19,7 @@ async function checkIfDisabled(bot, message, args, cmdname, channels) {
                         return(false)
                 }
         }
-        
+        }
 }
 async function findthemessage(bot, message, args, channels) {
         const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({
