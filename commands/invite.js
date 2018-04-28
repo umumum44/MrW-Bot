419907e49c77814cf6f5d82afc19c119078759cc
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-27 20:24:49 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,20 +1,7 @@
 const Discord = require("discord.js");
-async function checkIfDisabled(bot, message, args, cmdname, channels) {
-                const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({
-                        limit: 100
-                })))
-                const flatMessages = nestedMessages.reduce((a, b) => a.concat(b))
-                const msg = flatMessages.find(msg => msg.content.startsWith(`${message.guild.id} ${cmdname}`))
-		if(msg) {
-			return(true)
-		} else {
-			return(false)
-		}
-}
+
 module.exports.run = async (bot, message, args, prefix, content) => {
-         let channels = dbguild.channels.filter(m => RegExp("wbotdisable-database", "gi").test(m.name));
-	let cmddisablecheck = await checkIfDisabled(bot, message, args, "invite", channels)
-	if(cmddisablecheck) return message.reply("This command has been disabled by a server manager!")
+         
         message.reply("**Invite me here!:** https://discordapp.com/oauth2/authorize?client_id=419881218784493588&permissions=347201&scope=bot")
 }
 module.exports.help = {
