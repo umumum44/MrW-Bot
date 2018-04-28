ae1fa2fd56182adf7161dace5308cfb1b0ffe4d5
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-27 20:29:14 -0400

    Update rps.js

--- commands/rps.js
+++ commands/rps.js
@@ -1,20 +1,7 @@
 const Discord = require("discord.js");
-async function checkIfDisabled(bot, message, args, cmdname, channels) {
-                const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({
-                        limit: 100
-                })))
-                const flatMessages = nestedMessages.reduce((a, b) => a.concat(b))
-                const msg = flatMessages.find(msg => msg.content.startsWith(`${message.guild.id} ${cmdname}`))
-		if(msg) {
-			return(true)
-		} else {
-			return(false)
-		}
-}
+
 module.exports.run = async (bot, message, args) => {
-	 let channels = dbguild.channels.filter(m => RegExp("wbotdisable-database", "gi").test(m.name));
-	let cmddisablecheck = await checkIfDisabled(bot, message, args, "rps", channels)
-	if(cmddisablecheck) return message.reply("This command has been disabled by a server manager!")
+	
 	let choice = args[0];
 	var responses = ["rock", "paper", "scissors"];
 	if (!choice) return message.reply("You did not provide your choice!")
