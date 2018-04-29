095f6d9967a0db4e1cd40fce2c9c7c3c495c8a90
Windows <36823560+mrwagner27@users.noreply.github.com>
2018-04-28 22:00:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bot.on("ready", async () => {
         await tchannel.bulkDelete(100)
         let ttchannel = bot.channels.find(`id`, "437397457073078272");
         await ttchannel.bulkDelete(100)
-        await bot.user.setActivity("Servers!", {
+        await bot.user.setActivity("Servers", {
                 type: "WATCHING"
         });
         bot.channels.get("436947091483262996")
