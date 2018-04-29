6496c8cc82ce78b5595adfce4b6e58e4b660ec86
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-28 21:29:55 -0400

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -4,7 +4,16 @@ google.resultsPerPage = 3
 module.exports.run = async (bot, message, args, prefix, content) => {
        google(args[0], function (err, res){
   if (err) console.error(err)
- message.channel.send(`**Results:**\nTitle: ${res[0].title}\nLink: ${res[0].link}\nDescription: ${res[0].description}\n-----------------\nTitle: ${res[1].title}\nLink: ${res[1].link}\nDescription: ${res[1].description}\n-----------------\nTitle: ${res[2].title}\nLink: ${res[2].link}\nDescription: ${res[2].description}`)
+              var linkuno = res.links[0]
+              var linkdos = res.links[2]
+              var linktres = res.links[3]
+if((linkuno) && (linkdos) && (linktres)) {
+ message.reply(`**Results:**\nTitle: ${linkuno.title}\nLink: ${linkuno.link}\nDescription: ${linkuno.description}\n-----------------\nTitle: ${linkdos.title}\nLink: ${linkdos.link}\nDescription: ${linkdos.description}\n-----------------\nTitle: ${linktres.title}\nLink: ${linktres.link}\nDescription: ${linktres.description}`)
+} else if (linkuno) {
+        message.reply(`**Result:**\nTitle: ${linkuno.title}\nLink: ${linkuno.link}\nDescription: ${linkuno.description}`)
+} else {
+       message.reply("Couldn't find anything with this search query!")
+}
 })
 }
 module.exports.help = {
