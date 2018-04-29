f311fc90a9b0e2d399a152510ce5ab71a85b18c5
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-28 21:22:49 -0400

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -2,9 +2,9 @@ const Discord = require("discord.js");
 var google = require('google')
 google.resultsPerPage = 3
 module.exports.run = async (bot, message, args, prefix, content) => {
-       google('args[0]', function (err, res){
+       google(args[0], function (err, res){
   if (err) console.error(err)
- console.log(res)
+ message.channel.send(`**Results:**\nTitle: ${res[0].title}\nLink: ${res[0].link}\nDescription: ${res[0].description}\n-----------------\nTitle: ${res[1].title}\nLink: ${res[1].link}\nDescription: ${res[1].description}\n-----------------\nTitle: ${res[2].title}\nLink: ${res[2].link}\nDescription: ${res[2].description}`)
 })
 }
 module.exports.help = {
