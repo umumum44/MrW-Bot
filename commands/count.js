94f6fc22fd36143fc484216b64b26203abaca609
Windows <36823560+mrwagner27@users.noreply.github.com>
2018-04-30 17:27:36 -0400

    Update count.js

--- commands/count.js
+++ commands/count.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, prefix, content) => {
         
-        message.reply(`${bot.guilds.size} servers!!!`);
+        message.reply(`${bot.guilds.size} servers!!`);
 }
 module.exports.help = {
         name: "count"
