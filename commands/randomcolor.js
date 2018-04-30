4ce72be755d1961a86a48a2ca8b53b374724494e
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-29 21:01:16 -0400

    Update randomcolor.js

--- commands/randomcolor.js
+++ commands/randomcolor.js
@@ -1,15 +1,13 @@
 var randomHex = require('random-hex');
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix, content) => {
-	randomHex.generate().then((hex) => {
-		const embed = new Discord.RichEmbed()
-			.setTitle("Random Color")
-			.setColor(hex)
-			.setFooter(`Requested by ${message.author.tag}`)
-			.setDescription(`Hex Code: ${hex}`);
-		message.channel.send(embed).catch(function() {});
-	}).catch(function() {});
-	
+	var hex = randomHex.generate();
+	const embed = new Discord.RichEmbed()
+		.setTitle("Random Color")
+		.setColor(hex)
+		.setFooter(`Requested by ${message.author.tag}`)
+		.setDescription(`Hex Code: ${hex}`);
+	message.channel.send(embed).catch(function() {});	
 }
 module.exports.help = {
         name: "randomcolor"
