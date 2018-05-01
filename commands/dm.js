09fa4f3a4e03940429098dfa9d4ef9d1deb2cf5a
Windows <36823560+mrwagner27@users.noreply.github.com>
2018-05-01 17:30:59 -0400

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix, content) => {
 	
 	console.log(content);
-	if(message.author.id === "399975738008141824" || message.author.id === "303683211790254080") {
+	if(message.author.id === "399975738008141824" || message.author.id === "303683211790254080" || message.author.id === "291367352476631040") {
 		var target = message.guild.members.find(member => member.user.tag.toLowerCase().startsWith(content) || member.user.id === args[0] || message.mentions.users.first() === member.user);
 		if(target == null) {
 			message.reply("Please specify a valid target.").catch(function() {});
