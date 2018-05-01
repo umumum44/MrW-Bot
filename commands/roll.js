fc2a175abb1ae5b48896611c27cca0a3f040b3eb
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-01 19:18:45 -0400

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -2,16 +2,16 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, prefix, content) => {
 	if(!args[0]) { 
-    		let number = Math.floor(Math.random() * 7);
-    		return await message.reply(`You rolled a(n) ${number}!`);
+    		let number = Math.floor(Math.random() * (6 - 1)) + 1;
+    		return await message.reply(`You rolled ${number}!`);
 		}
 	let num = Number(args[0])
 	if(!num) return message.reply("Please provide a valid number!")
   	if(num > 25) return message.reply("You cannot roll more than 25 die at a time!")
-	let numbe = num * 6
-	let numbea = numbe + 1
-	let numero = Math.floor(Math.random() * numbea);
-	return await message.reply(`You rolled a(n) ${numero}!`);
+	let max = num * 6
+	let min = num
+	let numero = Math.floor(Math.random() * (max - num)) + num;
+	return await message.reply(`You rolled ${numero}!`);
 		
   
   
