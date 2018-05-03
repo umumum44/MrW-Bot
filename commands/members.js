ead2a967b60441d3b6779decb1b5fb934391b8a7
Yikey <gtc122004@gmail.com>
2018-05-03 17:54:41 -0400

    Update members.js

--- commands/members.js
+++ commands/members.js
@@ -3,15 +3,15 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix, content) => {
 	var memberEmbed = new Discord.RichEmbed().setColor("ORANGE");
 	var members;
-	var content = call.params.readRaw();
-	if (call.message.guild.roles.find(r => r.name.toLowerCase().startsWith(content.toLowerCase()))) {
+	var content = params.readRaw();
+	if (message.guild.roles.find(r => r.name.toLowerCase().startsWith(content.toLowerCase()))) {
 		if (content !== "") {
-			members = call.message.guild.roles.find(r => r.name.toLowerCase().startsWith(content.toLowerCase())).members.map(m => m.user.tag).sort()
-				.map(u => call.client.users.find(r => r.tag === u).toString()).join("\n");
-			memberEmbed.setTitle(`Users in ${call.message.guild.roles.find(r => r.name.toLowerCase().startsWith(content.toLowerCase())).name}`);
+			members = message.guild.roles.find(r => r.name.toLowerCase().startsWith(content.toLowerCase())).members.map(m => m.user.tag).sort()
+				.map(u => bot.users.find(r => r.tag === u).toString()).join("\n");
+			memberEmbed.setTitle(`Users in ${message.guild.roles.find(r => r.name.toLowerCase().startsWith(content.toLowerCase())).name}`);
 		} else {
-			members = call.message.guild.members.map(m => m.user.tag).sort()
-				.map(u => call.client.users.find(r => r.tag === u).toString()).join("\n");
+			members = message.guild.members.map(m => m.user.tag).sort()
+				.map(u => bot.users.find(r => r.tag === u).toString()).join("\n");
 			memberEmbed.setTitle("Users");
 		}
 		var membersLength = members.length;
@@ -28,18 +28,18 @@ module.exports.run = async (bot, message, args, prefix, content) => {
 				totalPages = totalPages / 20;
 			}
 			memberEmbed.setDescription(membersToSend.join("\n")).setFooter(`Page ${page}/${totalPages}`);
-			call.message.channel.send({
+			message.channel.send({
 				embed: memberEmbed
 			}).then(async function(sentEmbed) {
 				const emojiArray = ["◀", "▶"];
-				const filter = (reaction, user) => emojiArray.includes(reaction.emoji.name) && user.id === call.message.author.id;
+				const filter = (reaction, user) => emojiArray.includes(reaction.emoji.name) && user.id === message.author.id;
 				await sentEmbed.react(emojiArray[0]).catch(function() {});
 				await sentEmbed.react(emojiArray[1]).catch(function() {});
 				var reactions = sentEmbed.createReactionCollector(filter, {
 					time: 120000
 				});
 				reactions.on("collect", async function(reaction) {
-					await reaction.remove(call.message.author);
+					await reaction.remove(message.author);
 					if (reaction.emoji.name === "◀") {
 						if (page !== 1) {
 							page = page - 1;
@@ -56,27 +56,27 @@ module.exports.run = async (bot, message, args, prefix, content) => {
 
 					memberEmbed = new Discord.RichEmbed().setDescription(membersToSend.join("\n")).setColor("ORANGE")
 						.setFooter(`Page ${page}/${totalPages}`);
-					if (content !== "") memberEmbed.setTitle(`Users in ${call.message.guild.roles.find(r => r.name.toLowerCase().startsWith(content.toLowerCase())).name}`);
+					if (content !== "") memberEmbed.setTitle(`Users in ${message.guild.roles.find(r => r.name.toLowerCase().startsWith(content.toLowerCase())).name}`);
 					if (content === "") memberEmbed.setTitle("Users");
 					sentEmbed.edit(memberEmbed).catch(function() {});
 				});
 				reactions.on("end", () => sentEmbed.edit("Interactive command ended: 2 minutes passed."));
 			}).catch(() => {
-				call.message.reply("There was an error while trying to send this embed.").catch(() => {
-					call.message.author.send(`You attempted to run the \`members\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
+				message.reply("There was an error while trying to send this embed.").catch(() => {
+					message.author.send(`You attempted to run the \`members\` command in ${message.channel}, but I can not chat there.`).catch(function() {});
 				});
 			});
 		} else {
 			memberEmbed.setDescription(members);
-			call.message.channel.send({ embed: memberEmbed }).catch(() => {
-				call.message.reply("There was an error while trying to send this embed.").catch(() => {
-					call.message.author.send(`You attempted to run the \`members\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
+			message.channel.send({ embed: memberEmbed }).catch(() => {
+				message.reply("There was an error while trying to send this embed.").catch(() => {
+					message.author.send(`You attempted to run the \`members\` command in ${message.channel}, but I can not chat there.`).catch(function() {});
 				});
 			});
 		}
 	} else {
-		call.message.reply("Please specify a valid role, or supply no parameter for everyone in this server.").catch(() => {
-			call.message.author.send(`You attempted to run the \`members\` command in ${call.message.channel}, but I can not chat there.`).catch(function() {});
+		message.reply("Please specify a valid role, or supply no parameter for everyone in this server.").catch(() => {
+			message.author.send(`You attempted to run the \`members\` command in ${message.channel}, but I can not chat there.`).catch(function() {});
 		});
 	}
 }
