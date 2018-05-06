5345bb0992b9dda15a6c1a248b1b3cc242af70c9
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-06 15:17:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -210,6 +210,30 @@ bot.on("guildDelete", guildo => {
                 type: "WATCHING"
         });
 });
+bot.on("messageDelete", (message) => {
+		var messageAttachments = "";
+		if (message.attachments.first() !== undefined) messageAttachments = `\n${message.attachments.first().url}`;
+		if (message.content !== "" || messageAttachments !== "") {
+			var logsDatabase = bot.channels.get("440238037201453056");
+			logsDatabase.fetchMessages({ limit: 100 }).then(messages => {
+				messages.forEach(msg => {
+					var logChannel = bot.channels.get(msg.split(" ")[1]);
+					if (logChannel == undefined) return msg.delete();
+					var logGuild = logChannel.guild;
+					if (logGuild == undefined) return msg.delete();
+					if (logGuild.id === msg.guild.id) {
+						const messageDeleteEmbed = new Discord.RichEmbed()
+              .setTitle("Message Delete")
+              .setColor("RED")
+              .addField("Message Content", `Message ID: \`${message.id}\`\nMessage author: \`${message.author}\`\nMessage channel: ${message.channel}\nCreated at: \`${message.createdAt}\`\nDeleted at: \`${new Date.UTC()}\``)
+              .setDescription("``` ```" + `${message.content}${messageAttachments}` + "``` ```");
+            logsDatabase.send({ embed: messageDeleteEmbed }).catch(function() {});
+					}
+				});
+			});
+		}
+	});
+}
 bot.on("message", async message => {
         if (message.channel.type === "dm") return;
         if (message.channel.id === "437397457073078272") await message.delete(240000)
