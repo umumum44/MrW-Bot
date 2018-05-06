84cb217f176d6fd6a1fcaa67f98dc559aa2998ce
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-06 19:09:41 -0400

    Update logs.js

--- commands/logs.js
+++ commands/logs.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, message, args, prefix, content) => {
 	if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You don't have permission to use this command!");
         var logschannel = bot.channels.find(`id`, `440238037201453056`);
-	var msgs = logschannel.fetchMessages({ limit: 100 });
+	var msgs = await logschannel.fetchMessages({ limit: 100 });
 	var checker = msgs.find(m => m.content.startsWith(`${message.guild.id}`));
 	if(!checker) {
 	//assume it is disabled
