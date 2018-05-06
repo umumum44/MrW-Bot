56d784d92f60cf5ad0433209af786fd8d70626a7
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-06 18:41:59 -0400

    Create logs.js

---
+++ commands/logs.js
@@ -0,0 +1,29 @@
+module.exports.run = async (bot, message, args, prefix, content) => {
+	if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You don't have permission to use this command!");
+        var logschannel = bot.channels.get("440238037201453056");
+	var msgs = logschannel.fetchMessages({ limit: 100 });
+	var checker = msgs.find(m => m.content.startsWith(`${message.guild.id}`));
+	if(!checker) {
+	//assume it is disabled
+		if(!message.mentions.channels) return message.reply("You must mention a valid channel!");
+		let channelid = message.mentions.channels.first().id;
+		await logschannel.send(`${message.guild.id} ${channelid}`);
+		return message.reply(`Enabled and set logs channel to <#${channelid}>!`);
+	} else {
+	//assume it is enabled
+		if(message.mentions.channels) {
+			//assume they are trying to switch channels
+			let schannelid = message.mentions.channels.first().id;
+			await logschannel.send(`${message.guild.id} ${schannelid}`);
+			return message.reply(`Changed logs channel to <#${schannelid}>!`);
+		} else {
+			//disable completely
+			checker.delete();
+			return message.reply("Disabled logs in this server!");
+		}
+	}
+
+}
+module.exports.help = {
+        name: "logs"
+}
