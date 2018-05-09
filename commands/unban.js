041d215e4cfde506ffa6858d7a18940d73c9af9c
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-08 21:07:48 -0400

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix, content) => {
         if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("You do not have permission to use this command!").catch(function() {});
 	if(!args[0]) return message.reply("You must provide the member's username/tag to unban!").catch(function() {});
-	message.guild.fetchBans.then((bans) => {
+	message.guild.fetchBans().then((bans) => {
 		let filteredbans = bans.filter(user => user.tag.startsWith(args[0]));
 		if(filteredbans.size > 1) return message.reply("Please be more specific with the username to unban!").catch(function() {});
 		if(filteredbans.size > 0) return message.reply("Couldn't find anyone with this username/tag!").catch(function() {});
