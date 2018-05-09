189378f1c2ae87f92f9c7d8203d350225554a9c7
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-08 21:11:13 -0400

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -4,10 +4,10 @@ module.exports.run = async (bot, message, args, prefix, content) => {
         if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("You do not have permission to use this command!").catch(function() {});
 	if(!args[0]) return message.reply("You must provide the member's username/tag to unban!").catch(function() {});
 	message.guild.fetchBans().then((bans) => {
-		let filteredbans = bans.filter(user => user.tag.startsWith(args[0]));
+		let filteredbans = bans.filter(user => user.username.startsWith(args[0]));
 		console.log(filteredbans)
 		if(filteredbans.size > 1) return message.reply("Please be more specific with the username to unban!").catch(function() {});
-		if(filteredbans.size > 0) return message.reply("Couldn't find anyone with this username/tag!").catch(function() {});
+		if(filteredbans.size > 0) return message.reply("Couldn't find anyone with this username!").catch(function() {});
 		let banneduser = filteredbans.first();
 		message.guild.unban(banneduser).then(() => {
 			message.reply(`Successfully unbanned \`${banneduser.tag}\``).catch(function() {});
@@ -22,7 +22,7 @@ module.exports.run = async (bot, message, args, prefix, content) => {
 							const banEmbed = new Discord.RichEmbed()
 								.setTitle("Member Unbanned")
 								.setColor("RED")
-								.addField("Unban Information", `Unbanned ID: \`${banneduser.id}\`\nMember Unbanned: ${banneduser.tag}\nUnbanned At: \`${new Date(Date.now())}\`\nModerator: ${message.author}`)
+								.addField("Unban Information", `Unbanned ID: \`${banneduser.id}\`\nMember Unbanned: ${banneduser.username}\nUnbanned At: \`${new Date(Date.now())}\`\nModerator: ${message.author}`)
 							logChannel.send({ embed: banEmbed }).catch(function() {});
 						}
 					});
