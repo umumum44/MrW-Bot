9e345d7bf114649de1eadb07f767bd05282aa266
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-08 21:09:18 -0400

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -5,6 +5,7 @@ module.exports.run = async (bot, message, args, prefix, content) => {
 	if(!args[0]) return message.reply("You must provide the member's username/tag to unban!").catch(function() {});
 	message.guild.fetchBans().then((bans) => {
 		let filteredbans = bans.filter(user => user.tag.startsWith(args[0]));
+		console.log(filteredbans)
 		if(filteredbans.size > 1) return message.reply("Please be more specific with the username to unban!").catch(function() {});
 		if(filteredbans.size > 0) return message.reply("Couldn't find anyone with this username/tag!").catch(function() {});
 		let banneduser = filteredbans.first();
