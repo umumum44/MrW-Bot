64ad33d0f6d39484ac7884a918a705961760d5b1
Yikey <gtc122004@gmail.com>
2018-05-11 17:17:09 -0400

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,40 +2,19 @@ const Discord = require("discord.js");
 
 
 module.exports.run = async (bot, message, args) => {
-
- 
-
-
-
-
-
-    let boticon = bot.user.displayAvatarURL
-
-    let botinfocmd = new Discord.RichEmbed()
-
-    .setTitle("Bot Stats")
-
-    .setColor("#38F520")
-
-    .setThumbnail(boticon)
-
-    .addField("Bot Name", bot.user.username)
-
-    .addField("Created On", bot.user.createdAt)
-
-    .addField("Created By", "Windows 10 MacOS#0001 The Wonderful people who coded the bot ethanlaj#8805 and gt_c#0495");
-    
-
-
-
-    return message.channel.send(botinfocmd);
-
-  }
+	let boticon = bot.user.displayAvatarURL
+	let botinfocmd = new Discord.RichEmbed()
+		.setTitle("Bot Stats")
+		.setColor("#38F520")
+		.setThumbnail(boticon)
+		.addField("Bot Name", bot.user.username)
+		.addField("Created On", bot.user.createdAt)
+		.addField("Created By", "Windows 10 MacOS#0001 The Wonderful people who coded the bot ethanlaj#8805 and gt_c#0495");
+	return message.channel.send({ embed: botinfocmd }).catch(function() {});
+}
 
 
 
 module.exports.help = {
-
-    name: "botinfo"
-
+	name: "botinfo"
 }
