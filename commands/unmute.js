ac4138ab6fbbe5cc8c9bddc1c431f672bf6b7c36
Yikey <gtc122004@gmail.com>
2018-05-11 17:47:08 -0400

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,39 +1,39 @@
 const Discord = require("discord.js");
 const ms = require("ms");
 module.exports.run = async (bot, message, args) => {
-        let name = `${args[0]}`
-        if (message.member.hasPermission("KICK_MEMBERS")) {
-                let tounmute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-                if (!tounmute) {
-                        marray = message.guild.members.filter(m => RegExp(name, "gi")
-                                .test(m.displayName))
-                        tounmute = marray.first()
-                }
-                if (!tounmute) return message.reply("Couldn't find this user.")
-                if (message.member.highestRole.position <= tounmute.highestRole.position) return message.reply("This user is too high up in this guilds' hierarchy to be unmuted by you!");
-                let muterole = message.guild.roles.find(`name`, "Muted");
-                if (tounmute.roles.has(muterole.id)) {
-                        tounmute.removeRole(muterole)
-                        var logsDatabase = bot.channels.get("440238037201453056");
+	let name = `${args[0]}`
+	if (message.member.hasPermission("KICK_MEMBERS")) {
+		let tounmute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+		if (!tounmute) {
+			marray = message.guild.members.filter(m => RegExp(name, "gi")
+				.test(m.displayName))
+			tounmute = marray.first()
+		}
+		if (!tounmute) return message.reply("Couldn't find this user.")
+		if (message.member.highestRole.position <= tounmute.highestRole.position) return message.reply("This user is too high up in this guilds' hierarchy to be unmuted by you!");
+		let muterole = message.guild.roles.find(`name`, "Muted");
+		if (tounmute.roles.has(muterole.id)) {
+			tounmute.removeRole(muterole)
+			var logsDatabase = bot.channels.get("443931379907166210");
 			logsDatabase.fetchMessages({ limit: 100 }).then(logmessages => {
-			logmessages.forEach(msg => {
-				var logChannel = bot.channels.get(msg.content.split(" ")[1]);
-				if (logChannel == undefined) return msg.delete();
-				var logGuild = logChannel.guild;
-				if (logGuild == undefined) return msg.delete();
-				if (`${logGuild.id}` === `${message.guild.id}`) {
-				        const unmuteEmbed = new Discord.RichEmbed()
-					        .setTitle("Member Unmuted")
-					        .setColor("RED")
-					        .addField("Unmute Information", `Unmuted ID: \`${tounmute.id}\`\nMember Unmuted: ${tounmute}\nModerator: ${message.author}\nUnmuted At: \`${new Date(Date.now())}\``)
-					logChannel.send({ embed: unmuteEmbed }).catch(function() {});
-								}
-							});
-							});
-                        message.react("\u2705")
-                } else return message.reply("This user is not muted!")
-        }
+				logmessages.forEach(msg => {
+					var logChannel = bot.channels.get(msg.content.split(" ")[1]);
+					if (logChannel == undefined) return msg.delete();
+					var logGuild = logChannel.guild;
+					if (logGuild == undefined) return msg.delete();
+					if (`${logGuild.id}` === `${message.guild.id}`) {
+						const unmuteEmbed = new Discord.RichEmbed()
+							.setTitle("Member Unmuted")
+							.setColor("RED")
+							.addField("Unmute Information", `Unmuted ID: \`${tounmute.id}\`\nMember Unmuted: ${tounmute}\nModerator: ${message.author}\nUnmuted At: \`${new Date(Date.now())}\``)
+						logChannel.send({ embed: unmuteEmbed }).catch(function() {});
+					}
+				});
+			});
+			message.react("\u2705")
+		} else return message.reply("This user is not muted!")
+	}
 }
 module.exports.help = {
-        name: "unmute"
+	name: "unmute"
 }
