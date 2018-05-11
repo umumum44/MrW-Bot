6339eed2767a70ad6cfeba3d6e7d4391f8f31f63
Yikey <gtc122004@gmail.com>
2018-05-11 17:44:34 -0400

    Update setnews.js

--- commands/setnews.js
+++ commands/setnews.js
@@ -1,13 +1,13 @@
 module.exports.run = async (bot, message, args, prefix) => {
-        let guild = bot.guilds.find(`id`, "410400562232819723")
+        let guild = bot.guilds.get("410400562232819723")
         let member = await guild.fetchMember(message.author.id)
         if (!member) return;
         if (member.roles.get("410481036162760722")) { //owner 
                 let update = message.content.substr(prefix.length + 8);
-                let channel = bot.channels.find(`id`, "430477691154595852")
-                let editor = await channel.fetchMessage("430478034160713728")
-                await editor.edit(update)
-                await message.react("\u2705")
+                let channel = bot.channels.get("443931372508151820");
+                let editor = await channel.fetchMessage("443935229548167174");
+                await editor.edit(update);
+                await message.react("\u2705");
         }
 }
 module.exports.help = {
