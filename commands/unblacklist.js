048948eec1d184160cb56b4d9d8a5e9c315f3208
Yikey <gtc122004@gmail.com>
2018-05-11 17:46:15 -0400

    Update unblacklist.js

--- commands/unblacklist.js
+++ commands/unblacklist.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-        let guild = bot.guilds.find(`id`, "410400562232819723")
+        let guild = bot.guilds.get("410400562232819723")
         let member = await guild.fetchMember(message.author.id)
         if (!member) return;
         if (member.roles.get("410546480307503124") //mod
                 || member.roles.get("410611296401358848") //admin
                 || member.roles.get("410608939139334184") //coowner
                 || member.roles.get("410481036162760722")) { //owner 
-                let channel = bot.channels.find(`id`, "424006591411519499")
+                let channel = bot.channels.get("443931370968973312")
                 let pingeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
                 let userid = args[0]
                 let messages = await channel.fetchMessages({
