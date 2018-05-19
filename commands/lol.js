cde2e06198d5f6d69cccedd16a22173e53a05c8e
Yikey <gtc122004@gmail.com>
2018-05-19 14:59:07 -0400

    Create lol.js

---
+++ commands/lol.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args, prefix, content) => {
+        console.log(bot.databases.disabled);
+}
+module.exports.help = {
+        name: "test"
+}
