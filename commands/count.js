427571d118d8612765db5c89c7a1502c5f55fc00
Yikey <gtc122004@gmail.com>
2018-05-19 12:02:27 -0400

    Update count.js

--- commands/count.js
+++ commands/count.js
@@ -1,9 +1,8 @@
-const Discord = require("discord.js");
-
 module.exports.run = async (bot, message, args, prefix, content) => {
-        
         message.reply(`${bot.guilds.size} servers!!`);
 }
+
 module.exports.help = {
-        name: "count"
+        name: "count",
+        aliases: ["servers"]
 }
