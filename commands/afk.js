331b879224107c27c761a8b030e5530cce10650a
Yikey <gtc122004@gmail.com>
2018-05-19 12:38:38 -0400

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args, prefix, content) => {
         if (!afkmsg) afkmsg = "No reason provided.";
         let channel = bot.channels.get("443931374940979208");
         channel.send(`${message.author.id} ${afkmsg}`).catch(function() {});
-        message.reply(`You are now AFK!!!: \`${afkmsg}\`\nTo become un-AFK, just talk again.`).catch(function() {});
+        message.reply(`You are now AFK!!!: \`${afkmsg}\`\nTo become un-AFK, just talk again.`).then(msg => msg.delete(5000)).catch(function() {});
         message.delete().catch(function() {});
 }
 module.exports.help = {
