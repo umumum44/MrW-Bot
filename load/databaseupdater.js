8a3180f250d0bb0f2ec95b21815ed464674a771d
Yikey <gtc122004@gmail.com>
2018-05-19 15:12:02 -0400

    Update databaseupdater.js

--- load/databaseupdater.js
+++ load/databaseupdater.js
@@ -1,10 +1,19 @@
 module.exports.run = async (bot) => {
-	var achannels = bot.guilds.get("443929284411654144").channels.filter(m => RegExp("wbotprefixes-database", "gi").test(m.name));
-	const nestedMessages = await Promise.all(achannels.map(ch => ch.fetchMessages({ limit: 100 })));
+	var channels;
+	channels = bot.guilds.get("443929284411654144").channels.filter(m => RegExp("wbotprefixes-database", "gi").test(m.name));
+	const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({ limit: 100 })));
 	const flatMessages = nestedMessages.reduce((a, b) => a.concat(b))
 	flatMessages.forEach(msg => {
 		var guild = msg.content.split(" ")[0];
 		var prefix = msg.content.split(" ")[1];
-		bot.databases.prefix.push({ guild: guild, prefix: prefix });
+		bot.databases.prefixes.push({ guild: guild, prefix: prefix });
+	});
+	channels = bot.guilds.get("443929284411654144").channels.filter(m => RegExp("wbotdisable-database", "gi").test(m.name));
+	const nestedMessages = await Promise.all(channels.map(ch => ch.fetchMessages({ limit: 100 })));
+	const flatMessages = nestedMessages.reduce((a, b) => a.concat(b));
+	flatMessages.forEach(msg => {
+		var commands = msg.content.split(" ");
+		var guild = commands.shift();
+		bot.databases.disabled.push({ guild: guild, commands: commands });
 	});
 }
