aeccedf2560c602cfb80351f67b71179c61468fe
Yikey <gtc122004@gmail.com>
2018-05-19 14:57:08 -0400

    Update databaseupdater.js

--- load/databaseupdater.js
+++ load/databaseupdater.js
@@ -1,3 +1,10 @@
 module.exports.run = async (bot) => {
-  //soon :tm:
+  var achannels = bot.guilds.get("443929284411654144").channels.filter(m => RegExp("wbotprefixes-database", "gi").test(m.name));
+	const nestedMessages = await Promise.all(achannels.map(ch => ch.fetchMessages({ limit: 100 })));
+	const flatMessages = nestedMessages.reduce((a, b) => a.concat(b))
+	flatMessages.forEach(msg => {
+    var commands = msg.content.split(" ");
+    var guild = commands.shift();
+    bot.databases.disabled.push({ guild: guild, commands: commands });
+  });
 }
