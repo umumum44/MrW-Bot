ccb238e5c26cf9318e19975fef141326db029121
Yikey <gtc122004@gmail.com>
2018-05-19 16:58:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -46,18 +46,6 @@ bot.on("ready", async () => {
 bot.on("message", async message => {
 	if (message.channel.type === "dm") return;
 	if (message.author.bot) return;
-	let mention = message.mentions.members.first();
-	var checker;
-	if (mention) {
-		checker = bot.rateLimits.afk.find(value => value.user === mention.id);
-		if (checker) if (checker.user !== message.author.id)
-			message.reply(`This user is currently AFK!\nAFK Message: \`${checker.reason}\``).then(msg => msg.delete(5000));
-	}
-	checker = bot.rateLimits.afk.find(value => value.user === message.author.id);
-	if (checker) {
-		bot.rateLimits.afk.splice(bot.rateLimits.afk.indexOf(checker), 1);
-		message.reply("Welcome back! Your AFK status was removed.").then(msg => msg.delete(5000));
-	}
 	let cmd = message.content.split(" ")[0].toLowerCase();
 	if (!cmd) return;
 	let args = message.content.split(" ").slice(1);
