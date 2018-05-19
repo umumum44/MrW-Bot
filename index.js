fea067b4876b4822e2f78cf24345cedfcd7ec9ed
Yikey <gtc122004@gmail.com>
2018-05-19 17:15:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -55,19 +55,6 @@ bot.on("message", async message => {
 	var prefix = bot.databases.prefixes.find(value => value.guild === message.guild.id);
 	if (prefix != null) prefix = prefix.prefix; else prefix = botconfig.prefix;
 	if (message.isMemberMentioned(bot.user) && message.content.endsWith("prefix")) return message.reply(`My prefix is \`${prefix}\``);
-	if (message.isMemberMentioned(bot.user) && message.content.endsWith("prefix reset") && message.member.hasPermission("MANAGE_GUILD")) {
-		let aaa = dbguild.channels.filter(m => RegExp("wbotprefixes-database", "gi").test(m.name));
-		aaa.forEach(chl => {
-			chl.fetchMessages({ limit: 100 }).then(msgs => {
-				msgs.forEach(msg => {
-					if (msg.content.startsWith(`${message.guild.id}`)) {
-						msg.delete();
-					}
-				});
-			});
-		});
-		message.react("\u2705");
-	}
 	if (!message.content.startsWith(prefix)) return;
 	var commandfile = false;
 	bot.commands.forEach(command => {
