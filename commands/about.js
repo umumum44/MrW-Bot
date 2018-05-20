fb64806c75ce1b8e677929e0555a05c90c5f1654
Yikey <gtc122004@gmail.com>
2018-05-19 22:56:33 -0400

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -1,12 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, prefix, content) => {
-       {
-        let name = `${args[0]}`;
+        let name = args[0];
         let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
         if (!user) {
-                marray = message.guild.members.filter(m => RegExp(name, "gi")
-                        .test(m.displayName));
+                marray = message.guild.members.filter(m => RegExp(name, "gi").test(m.displayName));
                 user = marray.first();
         }
         if (!user) return message.channel.send("Couldn't find this user.")
@@ -20,9 +18,7 @@ module.exports.run = async (bot, message, args, prefix, content) => {
                 .addField("User ID", user.user.id)
                 .addField("Joined At", user.joinedAt)
                 .addField("Registered At", user.user.createdAt);
-        return message.channel.send({
-                embed: ab
-        });
+        return message.channel.send({ embed: ab });
 }
 module.exports.help = {
         name: "about"
