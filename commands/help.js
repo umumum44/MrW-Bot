deae7fdb152f1b3084f5c2749f4ddde0ef1aa916
Yikey <gtc122004@gmail.com>
2018-05-31 17:52:41 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,17 +2,17 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args, prefix, content) => {
 	var commandsEmbed = new Discord.RichEmbed()
 		.setTitle("Commands")
-                .setDescription("\`Default Bot Prefix:\` !!");
+		.setDescription("\`Default Bot Prefix:\` !!");
 	const TYPES = ["Fun", "Information", "Roles", "Moderation", "Miscellaneous", "Restricted"],
 		MAP = (command) => `\`${prefix}${command.help.name}\` - ${command.help.description}`;
 	TYPES.forEach(type => {
-                commandsEmbed.addField(type, bot.allcommands.filter(command => command.help.type === type).map(MAP));
-                });
-        message.author.send({ embed: commandsEmbed }).then(() => {
-                message.react("✅").catch(function () {});
-         }).catch(() => {
-                message.reply("I could not DM you the list of commands! Please check your privacy commands and try again!").catch(function () {});
-        });
+		commandsEmbed.addField(type, bot.allcommands.filter(command => command.help.type === type).map(MAP));
+	});
+	message.author.send({ embed: commandsEmbed }).then(() => {
+		message.react("✅").catch(function() {});
+	}).catch(() => {
+		message.reply("I could not DM you the list of commands! Please check your privacy commands and try again!").catch(function() {});
+	});
 }
 module.exports.help = {
 	name: "help",
