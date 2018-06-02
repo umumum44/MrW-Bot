2b0eca44433fcf29e9345e486834293982220cb5
Yikey <gtc122004@gmail.com>
2018-06-01 20:34:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -67,9 +67,7 @@ bot.on("message", async (message) => {
 			      channels = dbguild.channels.filter((m) => m.name.includes("wbotprefixes-database"));
 			var prefix = bot.databases.prefixes.find(value => value.guild === message.guild.id);
 			prefix = (message.content.startsWith(bot.user.toString())) ? bot.user.toString() : (prefix != null) ? prefix.prefix : botconfig.prefix;
-			cmd = cmd.slice(prefix.length);
-			console.log(cmd.length);
-			console.log(prefix.length);
+			cmd = (cmd === prefix) ? `${cmd} ${message.content.split(" ")[1].toLowerCase()}`.slice(prefix.length) : cmd.slice(prefix.length);
 			if (message.content.startsWith(prefix)) {
 				var commandFile = bot.commands.enabledCommands.find(command => command.help.name === cmd || (command.help.aliases || []).includes(cmd));
 				if (commandFile != null) {
