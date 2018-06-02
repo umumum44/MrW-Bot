be02abe981a5e38633d30516686d06749960a022
ethanlaj <ethanlaj@ericlaj.com>
2018-06-02 17:10:00 -0400

    prefix reset move

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,7 +1,23 @@
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message, args, oldprefix) => {
 	if (!message.member.hasPermission("MANAGE_GUILD")) return message.reply("You don't have permission to use this command!");
-	if (!args[0]) return message.reply("Please provide the new prefix!");
+	if (!args[0]) return message.reply(`My prefix is ${oldprefix}`);
 	let prefix = args[0];
+	if(prefix === "reset") {
+		let aaa = dbguild.channels.filter(m => RegExp("wbotprefixes-database", "gi").test(m.name));
+		aaa.forEach(chl => {
+			chl.fetchMessages({ limit: 100 }).then(msgs => {
+				msgs.forEach(msg => {
+					if (msg.content.startsWith(`${message.guild.id}`)) {
+						msg.delete();
+					}
+				});
+			}).catch(function () { });
+		});
+		let prefixDB = bot.databases.prefixes.find(value => value.guild === message.guild.id);
+		if (prefixDB != null) bot.databases.prefixes.splice(bot.databases.prefixes.indexOf(prefixDB), 1);
+		bot.databases.prefixes.push({ guild: message.guild.id, prefix: "!!" });
+		return message.react("\u2705");
+	}
 	if (prefix.length > 5) return message.reply("The prefix cannot be more than 5 characters!");
 	let dbguild = bot.guilds.get("443929284411654144");
 	let channel = dbguild.channels.find("name", "wbotprefixes-database");
