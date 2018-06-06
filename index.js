d5d1e4868eb99155e93b29dcc0c3de79ae90b657
Yikey <gtc122004@gmail.com>
2018-06-05 21:15:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -63,35 +63,8 @@ bot.on("message", (message) => {
 			var prefix = bot.databases.prefixes.find(value => value.guild === message.guild.id);
 			prefix = (prefix != null) ? prefix.prefix : botconfig.prefix;
 			cmd = cmd.slice(prefix.length);
-			let guild = bot.guilds.get("410400562232819723");
-			var permissionLevel = 0;
-			if (guild.members.get(message.author.id)) {
-				guild.fetchMember(message.author.id).then((member) => {
-					if (member.roles) {
-						if (member.roles.get("410546480307503124")) permissionLevel = 1;
-						if (member.roles.get("428563903937511426")) permissionLevel = 2;
-						if (member.roles.get("410611296401358848")) permissionLevel = 3;
-						if (member.roles.get("428283053828341783")) permissionLevel = 4;
-						if (member.roles.get("410608939139334184")) permissionLevel = 5;
-						if (member.roles.get("410481036162760722")) permissionLevel = 6;
-					}
-				}).catch(function () { });
-			}
-			if (message.author.id === "303683211790254080") permissionLevel = 7;
-			if (message.author.id === "399975738008141824") permissionLevel = 8;
-			/*Permission Guide
-			0 = Non-Member or Non-Matching Roles
-			Roles:
-			1 = Moderators
-			2 = Tech Admin
-			3 = Admin
-			4 = Head Admin
-			5 = Co-Owner
-			6 = Main Owner
-			Users:
-			7 = Mattify
-			8 = Windows
-			*/
+			var permissionLevel = bot.permissionLevel(message.author);
+			console.log(permissionLevel);
 			if (message.content.startsWith(prefix)) {
 				let commandFile = bot.commands.enabledCommands.find(command => command.help.name === cmd || (command.help.aliases || []).includes(cmd));
 				if (commandFile != null) {
