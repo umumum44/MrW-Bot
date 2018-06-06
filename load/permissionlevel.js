fe817a16f97d48902cd2791dbbf7c2212e7d491e
Yikey <gtc122004@gmail.com>
2018-06-05 21:16:49 -0400

    Update permissionlevel.js

--- load/permissionlevel.js
+++ load/permissionlevel.js
@@ -1,5 +1,5 @@
 module.exports.run = (bot) => {
-	bot.hasPermissionLevel = function(user) {
+	bot.getPermissionLevel = function(user) {
 		var guild = bot.guilds.has("410400562232819723");
 		var permissionLevel = 0;
 		if (guild.members.has(user.id)) {
